e4de0c7e906013da6208ef7a30d61a8582bb7fbb
Aravind Kumaresan <aravind.kumaresan@ft.com>
2012-10-04 12:56:29 +0100

    Removed the environment prefix from the build files.

--- main/webapp/WEB-INF/wro.groovy
+++ main/webapp/WEB-INF/wro.groovy
@@ -1,11 +1,11 @@
 groups {
-    "advertising-${version}-${APP.ENV}.min" {
+    "advertising-${version}.min" {
         js "/js/ft/**.js"
         js "/js/thirdparty/**.js"
         css "/css/ft/**.css"
     }
 
-    "advertising-${version}-${APP.ENV}" {
+    "advertising-${version}" {
         js(minimize: false, "/js/ft/**.js")
         js(minimize: false,"/js/thirdparty/**.js")
         css(minimize: false, "/css/ft/**.css")
