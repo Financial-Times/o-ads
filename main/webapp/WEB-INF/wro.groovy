346645fafa6c5957a5f94fd844583283b2f5d097
Robin Marr <robinemarr@gmail.com>
2012-10-24 15:30:43 +0100

    add advertising latest group to wro.groovy to create the files for deploy

--- main/webapp/WEB-INF/wro.groovy
+++ main/webapp/WEB-INF/wro.groovy
@@ -1,13 +1,21 @@
 groups {
-    "advertising-${version}.min" {
+    "advertising-latest" {
+        js(minimize: false, "/js/ft/**.js")
+        js(minimize: false,"/js/thirdparty/**.js")
+        css(minimize: false, "/css/ft/**.css")
+    }
+
+    "advertising-latest.min" {
         js "/js/ft/**.js"
         js "/js/thirdparty/**.js"
         css "/css/ft/**.css"
     }
 
+    "advertising-${version}.min" {
+        groupRef("advertising-latest")
+    }
+
     "advertising-${version}" {
-        js(minimize: false, "/js/ft/**.js")
-        js(minimize: false,"/js/thirdparty/**.js")
-        css(minimize: false, "/css/ft/**.css")
+        groupRef("advertising-latest.min")
     }
 }
