da9868a7dbaa56dc8f38cd61528ec90e33cc01b5
Robin Marr <robin.marr@ft,com>
2012-11-02 08:42:19 +0000

    Replacement jquery methods

---
+++ main/js/ft/advertising.utils.js
@@ -0,0 +1,145 @@
+(function (win, doc, FT, undef) {
+  "use strict";
+  FT._ads = FT.advertising || {};
+  var utils = {};
+
+  // Uses object prototype toString method to get at the type of object we are dealing,
+  // IE returns [object Object] for null and undefined so we need to filter those
+  // http://es5.github.com/#x15.2.4.2
+  function is(object, className) {
+    var type = Object.prototype.toString.call(object)
+      .match(/^\[object\s(.*)\]$/)[1];
+
+    if (object === null) {
+      return "Null";
+    } else if (object === undefined) {
+      return "Undefined";
+    } else {
+      return type;
+    }
+  }
+
+  function createIsTest(className){
+    return function (obj) {
+      return is(obj) === className;
+    };
+  }
+
+  function curryIsMethods(obj, classNames) {
+    classNames = classNames || [
+      "Array",
+      "Object",
+      "String",
+      "Function"
+    ];
+
+    while(!!classNames.length) {
+      var className = classNames.pop();
+      obj['is' + className] = createIsTest(className);
+    }
+
+    return obj;
+  }
+
+  utils = curryIsMethods(utils);
+
+  utils.isWindow = function (obj) {
+    return obj && obj !== null && obj == obj.window;
+  };
+
+  utils.isPlainObject = function (obj) {
+    var obj_hop = Object.prototype.hasOwnProperty;
+    // Must be an Object.
+    // Because of IE, we also have to check the presence of the constructor property.
+    // Make sure that DOM nodes and window objects don't pass through, as well
+    if (!obj || !utils.isObject(obj) || obj.nodeType || utils.isWindow(obj)) {
+        return false;
+    }
+
+    try {
+        // Not own constructor property must be Object
+        if ( obj.constructor &&
+            !obj_hop.call(obj, "constructor") &&
+            !obj_hop.call(obj.constructor.prototype, "isPrototypeOf") ) {
+            return false;
+        }
+    } catch ( e ) {
+        // IE8,9 Will throw exceptions on certain host objects
+        return false;
+    }
+
+    // Own properties are enumerated firstly, so to speed up,
+    // if last one is own, then all properties are own.
+
+    var key;
+    for ( key in obj ) {}
+
+    return key === undefined || obj_hop.call( obj, key );
+  };
+
+  utils.extend = function extend() {
+    var options, name, src, copy, copyIsArray, clone,
+        target = arguments[0] || {},
+        i = 1,
+        length = arguments.length,
+        deep = false;
+
+    // Handle a deep copy situation
+    if (typeof target === "boolean") {
+        deep = target;
+        target = arguments[1] || {};
+        // skip the boolean and the target
+        i = 2;
+    }
+
+    // Handle case when target is a string or something (possible in deep copy)
+    if (typeof target !== "object" && !utils.isFunction(target)) {
+        target = {};
+    }
+
+    // do nothing if only one argument is passed (or 2 for a deep copy)
+    if (length === i) {
+        return target;
+    }
+
+    for ( ; i < length; i++ ) {
+        // Only deal with non-null/undefined values
+        if ((options = arguments[i]) !== null) {
+            // Extend the base object
+            for (name in options) {
+                src = target[name];
+                copy = options[name];
+
+                // Prevent never-ending loop
+                if (target === copy) {
+                    continue;
+                }
+
+                // Recurse if we're merging arrays
+                if (deep && copy && (utils.isPlainObject(copy) || utils.isArray(copy))) {
+                    copyIsArray = utils.isArray(copy);
+                    if (copyIsArray) {
+                        copyIsArray = false;
+                        clone = src && utils.isArray(src) ? src : [];
+                    } else {
+                        clone = src && utils.isObject(src) ? src : {};
+                    }
+
+                    // Never move original objects, clone them
+                    target[name] = extend(deep, clone, copy);
+
+                // Don't bring in undefined values
+                } else if ( copy !== undefined ) {
+                    target[name] = copy;
+                }
+            }
+        }
+    }
+
+    // Return the modified object
+    return target;
+  };
+
+  FT._ads.utils = utils;
+  return utils;
+}(window, document, FT));
---
+++ test/js/advertising.utils.test.js
@@ -0,0 +1,252 @@
+(function (win, doc, FT, $, undef) {
+    "use strict";
+    function isTests() {
+        module('_ads Utilities is methods');
+
+        test('Utils object exists', function () {
+            expect(1);
+            ok($.type(FT._ads.utils) === 'object', 'The name space exists');
+        });
+
+        test('isArray method', function () {
+            expect(10);
+            ok($.type(FT._ads.utils.isArray) === 'function', 'The function exists');
+            ok(FT._ads.utils.isArray([]), 'Array returns true');
+            ok(!FT._ads.utils.isArray(arguments), 'Array like object returns false');
+            ok(!FT._ads.utils.isArray({}), 'Object returns false');
+            ok(!FT._ads.utils.isArray("hello"), 'String returns false');
+            ok(!FT._ads.utils.isArray(/hello/), 'Regex returns false');
+            ok(!FT._ads.utils.isArray(1e6), 'Number returns false');
+            ok(!FT._ads.utils.isArray(null), 'Null returns false');
+            ok(!FT._ads.utils.isArray(undef), 'Undefined returns false');
+            ok(!FT._ads.utils.isArray(function () {}), 'Undefined returns false');
+        });
+
+        test('isString method', function () {
+            expect(9);
+            ok($.type(FT._ads.utils.isString) === 'function', 'The function exists');
+            ok(FT._ads.utils.isString("hello"), 'String returns true');
+            ok(!FT._ads.utils.isString([]), 'Array returns false');
+            ok(!FT._ads.utils.isString({}), 'Object returns false');
+            ok(!FT._ads.utils.isString(/hello/), 'Regex returns false');
+            ok(!FT._ads.utils.isString(1e6), 'Number returns false');
+            ok(!FT._ads.utils.isString(null), 'Null returns false');
+            ok(!FT._ads.utils.isString(undef), 'Undefined returns false');
+            ok(!FT._ads.utils.isString(function () {}), 'Function returns false');
+        });
+
+        test('isFunction method', function () {
+            expect(9);
+            ok($.type(FT._ads.utils.isFunction) === 'function', 'The function exists');
+            ok(FT._ads.utils.isFunction(function () {}), 'Function returns true');
+            ok(!FT._ads.utils.isFunction("hello"), 'String returns false');
+            ok(!FT._ads.utils.isFunction([]), 'Array returns false');
+            ok(!FT._ads.utils.isFunction({}), 'Object returns false');
+            ok(!FT._ads.utils.isFunction(/hello/), 'Regex returns false');
+            ok(!FT._ads.utils.isFunction(1e6), 'Number returns false');
+            ok(!FT._ads.utils.isFunction(null), 'Null returns false');
+            ok(!FT._ads.utils.isFunction(undef), 'Undefined returns false');
+        });
+
+        test('isObject method', function () {
+            expect(9);
+            ok($.type(FT._ads.utils.isObject) === 'function', 'The function exists');
+            ok(FT._ads.utils.isObject({}), 'Object returns true');
+            ok(!FT._ads.utils.isObject(function () {}), 'Function returns false');
+            ok(!FT._ads.utils.isObject("hello"), 'String returns false');
+            ok(!FT._ads.utils.isObject([]), 'Array returns false');
+            ok(!FT._ads.utils.isObject(/hello/), 'Regex returns false');
+            ok(!FT._ads.utils.isObject(1e6), 'Number returns false');
+            ok(!FT._ads.utils.isObject(null), 'Null returns false');
+            ok(!FT._ads.utils.isObject(undef), 'Undefined returns false');
+        });
+
+        test("isWindow method", function() {
+            expect(14);
+            ok($.type(FT._ads.utils.isWindow) === 'function', 'The function exists');
+            ok(FT._ads.utils.isWindow(window), "window");
+            ok(!FT._ads.utils.isWindow(), "empty");
+            ok(!FT._ads.utils.isWindow(null), "null");
+            ok(!FT._ads.utils.isWindow(undefined), "undefined");
+            ok(!FT._ads.utils.isWindow(document), "document");
+            ok(!FT._ads.utils.isWindow(document.documentElement), "documentElement");
+            ok(!FT._ads.utils.isWindow(""), "string");
+            ok(!FT._ads.utils.isWindow(1), "number");
+            ok(!FT._ads.utils.isWindow(true), "boolean");
+            ok(!FT._ads.utils.isWindow({}), "object");
+            ok(!FT._ads.utils.isWindow({ setInterval: function(){} }), "fake window");
+            ok(!FT._ads.utils.isWindow(/window/), "regexp");
+            ok(!FT._ads.utils.isWindow(function(){}), "function");
+        });
+
+        test("isPlainObject method",function(){
+            expect(15);
+            ok($.type(FT._ads.utils.isPlainObject) === 'function', 'The function exists');
+
+            // The use case that we want to match
+            ok(FT._ads.utils.isPlainObject({}), "{}");
+
+            // Not objects shouldn't be matched
+            ok(!FT._ads.utils.isPlainObject(""), "string");
+            ok(!FT._ads.utils.isPlainObject(0) && !jQuery.isPlainObject(1), "number");
+            ok(!FT._ads.utils.isPlainObject(true) && !jQuery.isPlainObject(false), "boolean");
+            ok(!FT._ads.utils.isPlainObject(null), "null");
+            ok(!FT._ads.utils.isPlainObject(undefined), "undefined");
+
+            // Arrays shouldn't be matched
+            ok(!FT._ads.utils.isPlainObject([]), "array");
+
+            // Instantiated objects shouldn't be matched
+            ok(!FT._ads.utils.isPlainObject(new Date()), "new Date");
+
+            var fnplain = function(){};
+
+            // Functions shouldn't be matched
+            ok(!FT._ads.utils.isPlainObject(fnplain), "fn");
+
+            /** @constructor */
+            var fn = function() {};
+
+            // Again, instantiated objects shouldn't be matched
+            ok(!FT._ads.utils.isPlainObject(new fn()), "new fn (no methods)");
+
+            // Makes the function a little more realistic
+            // (and harder to detect, incidentally)
+            fn.prototype["someMethod"] = function(){};
+
+            // Again, instantiated objects shouldn't be matched
+            ok(!FT._ads.utils.isPlainObject(new fn()), "new fn");
+
+            // DOM Element
+            ok(!FT._ads.utils.isPlainObject(document.createElement("div")), "DOM Element");
+
+            // Window
+            ok(!FT._ads.utils.isPlainObject(window), "window");
+
+            try {
+                FT._ads.utils.isPlainObject( window.location );
+                ok( true, "Does not throw exceptions on host objects");
+            } catch ( e ) {
+                ok( false, "Does not throw exceptions on host objects -- FAIL");
+            }
+        });
+    }
+
+    function extendTests() {
+        module('_ads Utilities extend method');
+
+        test("FT._ads.utils.extend(Object, Object)", function() {
+            expect(27);
+
+            var settings = { "xnumber1": 5, "xnumber2": 7, "xstring1": "peter", "xstring2": "pan" },
+                options = { "xnumber2": 1, "xstring2": "x", "xxx": "newstring" },
+                optionsCopy = { "xnumber2": 1, "xstring2": "x", "xxx": "newstring" },
+                merged = { "xnumber1": 5, "xnumber2": 1, "xstring1": "peter", "xstring2": "x", "xxx": "newstring" },
+                deep1 = { "foo": { "bar": true } },
+                deep1copy = { "foo": { "bar": true } },
+                deep2 = { "foo": { "baz": true }},
+                deep2copy = { "foo": { "baz": true }},
+                deepmerged = { "foo": { "bar": true, "baz": true }},
+                arr = [1, 2, 3],
+                nestedarray = { "arr": arr };
+
+            FT._ads.utils.extend(settings, options);
+            deepEqual( settings, merged, "Check if extended: settings must be extended" );
+            deepEqual( options, optionsCopy, "Check if not modified: options must not be modified" );
+
+            FT._ads.utils.extend(settings, null, options);
+            deepEqual( settings, merged, "Check if extended: settings must be extended" );
+            deepEqual( options, optionsCopy, "Check if not modified: options must not be modified" );
+
+            FT._ads.utils.extend(true, deep1, deep2);
+            deepEqual( deep1["foo"], deepmerged["foo"], "Check if foo: settings must be extended" );
+            deepEqual( deep2["foo"], deep2copy["foo"], "Check if not deep2: options must not be modified" );
+            ok( FT._ads.utils.extend(true, {}, nestedarray)["arr"] !== arr, "Deep extend of object must clone child array" );
+
+            // #5991
+            ok( jQuery.isArray( FT._ads.utils.extend(true, { "arr": {} }, nestedarray)["arr"] ), "Cloned array have to be an Array" );
+            ok( jQuery.isPlainObject( FT._ads.utils.extend(true, { "arr": arr }, { "arr": {} })["arr"] ), "Cloned object have to be an plain object" );
+
+            var empty = {};
+            var optionsWithLength = { "foo": { "length": -1 } };
+            FT._ads.utils.extend(true, empty, optionsWithLength);
+            deepEqual( empty["foo"], optionsWithLength["foo"], "The length property must copy correctly" );
+
+            empty = {};
+            var optionsWithDate = { "foo": { "date": new Date() } };
+            FT._ads.utils.extend(true, empty, optionsWithDate);
+            deepEqual( empty["foo"], optionsWithDate["foo"], "Dates copy correctly" );
+
+            /** @constructor */
+            var myKlass = function() {};
+            var customObject = new myKlass();
+            var optionsWithCustomObject = { "foo": { "date": customObject } };
+            empty = {};
+            FT._ads.utils.extend(true, empty, optionsWithCustomObject);
+            ok( empty["foo"] && empty["foo"]["date"] === customObject, "Custom objects copy correctly (no methods)" );
+
+            // Makes the class a little more realistic
+            myKlass.prototype = { "someMethod": function(){} };
+            empty = {};
+            FT._ads.utils.extend(true, empty, optionsWithCustomObject);
+            ok( empty["foo"] && empty["foo"]["date"] === customObject, "Custom objects copy correctly" );
+
+            var MyNumber = Number;
+            var ret = FT._ads.utils.extend(true, { "foo": 4 }, { "foo": new MyNumber(5) } );
+            ok( ret.foo == 5, "Wrapped numbers copy correctly" );
+
+            var nullUndef;
+            nullUndef = FT._ads.utils.extend({}, options, { "xnumber2": null });
+            ok( nullUndef["xnumber2"] === null, "Check to make sure null values are copied");
+
+            nullUndef = FT._ads.utils.extend({}, options, { "xnumber2": undefined });
+            ok( nullUndef["xnumber2"] === options["xnumber2"], "Check to make sure undefined values are not copied");
+
+            nullUndef = FT._ads.utils.extend({}, options, { "xnumber0": null });
+            ok( nullUndef["xnumber0"] === null, "Check to make sure null values are inserted");
+
+            var target = {};
+            var recursive = { foo:target, bar:5 };
+            FT._ads.utils.extend(true, target, recursive);
+            deepEqual( target, { bar:5 }, "Check to make sure a recursive obj doesn't go never-ending loop by not copying it over" );
+
+            ret = FT._ads.utils.extend(true, { foo: [] }, { foo: [0] } ); // 1907
+            equal( ret.foo.length, 1, "Check to make sure a value with coersion 'false' copies over when necessary to fix #1907" );
+
+            ret = FT._ads.utils.extend(true, { foo: "1,2,3" }, { foo: [1, 2, 3] } );
+            ok( typeof ret.foo != "string", "Check to make sure values equal with coersion (but not actually equal) overwrite correctly" );
+
+            ret = FT._ads.utils.extend(true, { foo:"bar" }, { foo:null } );
+            ok( typeof ret.foo !== "undefined", "Make sure a null value doesn't crash with deep extend, for #1908" );
+
+            var obj = { foo:null };
+            FT._ads.utils.extend(true, obj, { foo:"notnull" } );
+            equal( obj.foo, "notnull", "Make sure a null value can be overwritten" );
+
+            function func() {}
+            FT._ads.utils.extend(func, { key: "value" } );
+            equal( func.key, "value", "Verify a function can be extended" );
+
+            var defaults = { xnumber1: 5, xnumber2: 7, xstring1: "peter", xstring2: "pan" },
+                defaultsCopy = { xnumber1: 5, xnumber2: 7, xstring1: "peter", xstring2: "pan" },
+                options1 = { xnumber2: 1, xstring2: "x" },
+                options1Copy = { xnumber2: 1, xstring2: "x" },
+                options2 = { xstring2: "xx", xxx: "newstringx" },
+                options2Copy = { xstring2: "xx", xxx: "newstringx" },
+                merged2 = { xnumber1: 5, xnumber2: 1, xstring1: "peter", xstring2: "xx", xxx: "newstringx" };
+
+            settings = FT._ads.utils.extend({}, defaults, options1, options2);
+            deepEqual( settings, merged2, "Check if extended: settings must be extended" );
+            deepEqual( defaults, defaultsCopy, "Check if not modified: options1 must not be modified" );
+            deepEqual( options1, options1Copy, "Check if not modified: options1 must not be modified" );
+            deepEqual( options2, options2Copy, "Check if not modified: options2 must not be modified" );
+        });
+    }
+
+    function runTests() {
+        isTests();
+        extendTests();
+    }
+
+    $(runTests);
+}(window, document, FT, jQuery));
