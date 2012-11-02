43bbe8317452b942f75b231b40c331251c81d514
Robin Marr <robin.marr@ft,com>
2012-11-02 08:06:29 +0000

    CHanges to only jshint our files

--- main/webapp/WEB-INF/wro.groovy
+++ main/webapp/WEB-INF/wro.groovy
@@ -1,13 +1,20 @@
 groups {
+    "jshint" {
+        js(minimize: false, "/js/ft/**.js")
+        js(minimize: false,"/js/thirdparty/**.js")
+    }
+
     "advertising-latest" {
         js(minimize: false, "/js/ft/**.js")
         js(minimize: false,"/js/thirdparty/**.js")
+        js(minimize: false,"/js/lib/**.js")
         css(minimize: false, "/css/ft/**.css")
     }
 
     "advertising-latest.min" {
         js "/js/ft/**.js"
         js "/js/thirdparty/**.js"
+        js "/js/lib/**.js"
         css "/css/ft/**.css"
     }
 
