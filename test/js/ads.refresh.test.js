c98efa1f02b82455c44fa9a253edc6bd2cfa14a6
Yolande Leung <yolande.leung@ft.com>
2013-03-21 12:02:15 +0000

    US29658 - Adding unit tests for the ads refresh code - review updates

--- test/js/ads.refresh.test.js
+++ test/js/ads.refresh.test.js
@@ -1,40 +1,15 @@
+/*xxxjshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, undef:true, unused:true, curly:true, browser:true, indent:4, maxerr:50 */
+/*xxxglobal
+    xxx$, FT, sinon, tests, module, test, deepEqual */
+
+/*xxxproperties
+    CONST, ads, asset, callCount, clock, delay, env, initDFP, refreshDelayMs,
+    reset, restore, setup, spy, spyPageRefresh, startRefreshTimer, stub,
+    stubReloadWindow, teardown, test, tick, useFakeTimers, userInteracting
+*/
+
+
 FT.ads.initDFP();
-
-
-if (typeof(FT._ads.utils.cookies.FTQA) !== "undefined") {
-
-    envVars = FT._ads.utils.cookies.FTQA.split("%3B");
-
-    for (j = 0; j < envVars.length; j++) {
-        if (re.test(envVars[j])) {
-            nameAndValue = envVars[j].split("%3D");
-            FT.env[nameAndValue[0]] = nameAndValue[1];
-            debugStr += nameAndValue[0] + ' = ' + nameAndValue[1] + ' ';
-        }
-    }
-
-    //now we find what test mode we are in and either call DFP (Integration) or Mock the ad call (unit)
-    testMode = unitOrIntegrationMode(FT._ads.utils.cookies.FTQA);
-
-    if (testMode === 'unit') {
-        //if unit test, then we override FT.lib.writeScript with the call to Mocking
-        FT._ads.utils.writeScript = function (URL) {
-            mockAdContent(URL, Tests); // Ads injected immediately
-        };
-
-        var Tests = {
-            'intro':TestMockAd.intro,
-            'banlb':TestMockAd.banlb3,
-            'newssubs':TestMockAd.newssubs2,
-            'tlbxrib':TestMockAd.tlbxrib,
-            'mktsdata':TestMockAd.mktsdata,
-            'hlfmpu':TestMockAd.hlfmpu,
-            'doublet':TestMockAd.doublet,
-            'refresh':TestMockAd.refresh
-        };
-    }
-
-}
 
 //set up test spies
 FT.env.asset = "page";
@@ -45,13 +20,13 @@ FT.test.stubReloadWindow = sinon.stub(FT.ads, "reloadWindow");
 function tests() {
 
     module("test-helpers", {
-        setup:function () {
+        setup: function () {
             FT.test.clock = sinon.useFakeTimers();
-            FT.test.delay = 720*1000;
+            FT.test.delay = 720 * 1000;
             FT.userInteracting = false;
         },
-        teardown:function () {
-            FT.test.clock = FT.test.clock.restore()
+        teardown: function () {
+            FT.test.clock = FT.test.clock.restore();
         }
     });
 
@@ -75,11 +50,11 @@ function tests() {
 
         FT.ads.startRefreshTimer(FT.test.delay);
 
-        FT.test.clock.tick(FT.test.delay*2);
+        FT.test.clock.tick(FT.test.delay * 2);
         deepEqual(FT.test.spyPageRefresh.callCount, 2, "Number of times spyPageRefresh() function should be called");
         deepEqual(FT.test.stubReloadWindow.callCount, 0, "Number of times stubReloadWindow() function should be called");
     });
 }
-;
+
 
 $(tests);
