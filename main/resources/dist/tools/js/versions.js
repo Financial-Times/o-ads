1bae07ce5e3a2899451e98f475cab76c60848788
Robin Marr <robin.marr@ft.com>
2013-05-15 15:39:14 +0100

    add versions tool to the menu on the tools page

--- main/resources/dist/tools/js/versions.js
+++ main/resources/dist/tools/js/versions.js
@@ -15,36 +15,46 @@
   function addVersionsToPage(data) {
     var availableRefs = data[gitRepo].availableRefs;
     availableRefs.forEach(function (item) {
-      if(regExp.tags.test(item)){
+      if (regExp.tags.test(item)) {
         var version = item.replace(regExp.tags,'');
-        $('<tr id="' + version + '"><td>' + version + '</td><td class="js waiting"></td><tr>')
+        $('<tr id="' + version + '"><td>' + version + '</td><td class="waiting"></td><td class="info"></td><tr>')
         .appendTo(versionsTbody);
         isVersionAvailable(version);
       }
     });
   }
 
-  function updateVersionAvailability(outcome, version) {
-    console.log(outcome, version);
-    var outcomeText = outcome === 'success' ? '✔' : '✘';
-    $(document.getElementById(version)).find('td.waiting')
-      .removeClass('waiting')
-      .addClass(outcome)
-      .text(outcomeText);
+  function updateVersionAvailability(outcome, version, file) {
+    var outcomeText = outcome === 'success' ? '✔' : '✘',
+      tableRow = $(document.getElementById(version)),
+      availability = tableRow.find('td.waiting'),
+      information = tableRow.find('td.info');
+
+      availability.removeClass('waiting')
+        .addClass(outcome)
+        .text(outcomeText);
+
+      if (file) {
+        var gitRev = file.match(regExp.gitRev)[1] || 'Missing',
+          buildLifeId = file.match(regExp.buildLifeId)[1] || 'Missing',
+          buildLifeDate = file.match(regExp.buildLifeDate)[1] || 'Missing';
+        information.html('gitRev: ' + gitRev + ' buildlife Id: ' + buildLifeId + ' buildLife Date: ' + buildLifeDate);
+      }
   }
 
   function isVersionAvailable(version) {
     $.get(advertisingBase + version + '.js')
       .always(function (response) {
-        if(response.status === 200) {
-          updateVersionAvailability('success', version);
+        if (response.status === 200) {
+          console.log(response);
+          updateVersionAvailability('success', version, response.responseText);
         } else {
           updateVersionAvailability('fail', version);
         }
       });
   }
 
-  function init(){
+  function init() {
     //get repo information from gitblit so we can see what versions exist
     $.getJSON(gitAPI + '?req=LIST_REPOSITORIES')
       .done(addVersionsToPage);
