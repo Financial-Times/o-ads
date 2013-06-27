148f78e0e528b152b808690503c7f4ec6d0c86b0
Robin Marr <robin.marr@ft.com>
2013-06-27 17:35:27 +0100

    US31899: add a basic config object and some tests for it
    Serve ads using GPT on HTSI

--- main/js/thirdparty/third.party.gpt.js
+++ main/js/thirdparty/third.party.gpt.js
@@ -5,7 +5,63 @@
  * @author Robin Marr, robin.marr@ft.com
  */
 
-(function (win, doc, FT, undefined) {
-  "use strict";
+(function (win, doc, undefined) {
+    "use strict";
 
-}(window, document, FT || {}));
+    function Config() {
+        var self = this,
+        defaults = {};
+
+        this.store = FT._ads.utils.extend({}, this.fetchMetaConfig(), this.fetchGlobalConfig());
+
+        return this;
+    }
+
+    Config.prototype.fetchMetaConfig = function() {
+        var meta,
+            results = {},
+            metas = doc.getElementsByTagName('meta');
+
+        while (meta = metas.pop()) {
+            if (meta.name) {
+                results[meta.name] = meta.content;
+            }
+        }
+        return results;
+    };
+
+    Config.prototype.fetchGlobalConfig = function(first_argument) {
+        return FT._ads.utils.isObject(FT.env) ? FT.env : {};
+    };
+
+    Config.prototype.get = function get(key) {
+        var result;
+        if (key) {
+            result = this.store[key];
+        } else {
+            result = this.store;
+        }
+
+        return result;
+    };
+
+    Config.prototype.set = function set(key, value) {
+        var result;
+        if (value) {
+            this.store[key] = value;
+            result = value;
+        }
+
+        return result;
+    };
+
+    if (!FT) {
+        FT = win.FT = {};
+    }
+
+    if (!FT.ads) {
+        FT.ads = {};
+    }
+
+    FT._ads.utils.extend(FT.ads, {config: new Config()});
+}(window, document));
