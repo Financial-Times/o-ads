d32bd63b93af829e852114fa753d3172ec669642
Andrew Georgiou <andrew.georgiou@ft.com>
2013-07-09 17:15:26 +0100

    Refactored the AudienceScience thirdparty library module for use with the GFP tags

--- main/js/thirdparty/third.party.audsci.js
+++ main/js/thirdparty/third.party.audsci.js
@@ -10,11 +10,35 @@
 
     function AudSci() {
         var self = this;
-
         return this;
     }
 
-    if (!FT) {
+    AudSci.prototype.getAudSci = function(cookie){
+        var audsciCookie,
+            rsiSegs;
+       function parseSegs(segs, max) {
+            var match,
+                exp = /J07717_10*([^|]*)/g,
+                prefix = ';a=z',
+                found = [],
+                result = '';
+            if (max) {
+                    while(match = exp.exec(segs)) {
+                    found.push(match[1]);
+                    }
+            if (found.length) {
+                    result = prefix  + found.slice(0, max).join(prefix);
+                }
+            }
+            return result;
+        }
+
+        audsciCookie = cookie === undefined ? document.cookie : cookie;
+        rsiSegs = audsciCookie.replace(/^.*\brsi_segs=([^;]*).*$/, '$1');
+        return parseSegs(rsiSegs, FT.ads.config.audSciLimit);
+    };
+
+    if (!win.FT ) {
         FT = win.FT = {};
     }
 
