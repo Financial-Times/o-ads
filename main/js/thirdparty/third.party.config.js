688fe2ce93f8731e0da885e5e4241e831663620b
Andrew Georgiou <andrew.georgiou@ft.com>
2013-07-25 13:47:05 +0100

    refactoring config

--- main/js/thirdparty/third.party.config.js
+++ main/js/thirdparty/third.party.config.js
@@ -10,8 +10,8 @@
 
     function Config() {
 
-        this.defaults = {
-          network: '15887',
+        var defaults = {
+          network: '5887',
           collapseEmpty: true,
           formats:  {
                 banlb: [[728,90], [468,60], [970,90]],
@@ -27,13 +27,10 @@
           audSciLimit : 2
         };
 
-        this.store = {};
-        this.init();
+       // this.store = {};
+    var store = {};
 
-        return this;
-    }
-
-    Config.prototype.fetchMetaConfig = function() {
+    var fetchMetaConfig = function() {
         var meta,
             results = {},
             metas = doc.getElementsByTagName('meta');
@@ -48,38 +45,42 @@
         return results;
     };
 
-    Config.prototype.fetchGlobalConfig = function() {
+    var fetchGlobalConfig = function() {
         return FT._ads.utils.isObject(FT.env) ? FT.env : {};
     };
 
-    Config.prototype.get = function get(key) {
+    var access= function(k,v){
         var result;
-        if (key) {
-            result = this.store[key];
-        } else {
-            result = this.store;
+        if (typeof v==="undefined") {
+            // envoke the getter
+            if (typeof k ==="undefined"){result = store;}
+            else {
+                if (k === 'clear') {
+                    store={};
+                    result=store;
+                }
+                else {result = store[k];}
+            }
         }
-
-        return result;
-    };
-
-    Config.prototype.set = function set(key, value) {
-        var result;
-        if (value) {
-            this.store[key] = value;
-            result = value;
+        if (typeof v !== "undefined"){
+            store[k] = v;
+            result = v;
         }
+    return result;
+  };
 
-        return result;
+
+    var clear = function() {
+        store = {};
     };
 
-    Config.prototype.clear = function clear() {
-        this.store = {};
+    var init = function() {
+        store = FT._ads.utils.extend({}, defaults, fetchMetaConfig(), fetchGlobalConfig());
     };
 
-    Config.prototype.init = function init() {
-        this.store = FT._ads.utils.extend({}, this.defaults, this.fetchMetaConfig(), this.fetchGlobalConfig());
-    };
+   init();
+        return access;
+    }
 
     if (!win.FT && FT._ads.utils.isObject(win.FT)) {
         FT = win.FT = {};
