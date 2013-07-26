cf6c004e994e38f6ed9c35faa96fe35283007e45
Robin Marr <robin.marr@ft.com>
2013-07-26 14:42:58 +0100

    US31899: Refactor of GPT module to allow it to run before DOM ready
    Serve ads using GPT on HTSI

--- main/js/thirdparty/third.party.switcher.js
+++ main/js/thirdparty/third.party.switcher.js
@@ -12,18 +12,22 @@
       var mode = FT.ads.config('ftads:mode');
       if (mode === 'gpt') {
           FT.env.adCall = function () {
-            return false;
+            FT.ads.gpt.displaySlot.apply(FT.ads.gpt, arguments);
           };
 
           // for HTSI
           FT.env.advert = function () {
-            return false;
+            FT.ads.gpt.displaySlot.apply(FT.ads.gpt, arguments);
           };
 
           FT.ads.config('formats', { banlb: [[728,90], [468,60], [970,90]], mpu: [[300,250],[336,280]], doublet: [[342,200]], hlfmpu: [[300,600],[336,850],[300,250],[336,280]], intro: [[1,1]], newssubs: [[239,90]], refresh: [[1,1]], searchbox: [[200,28]], tlbxrib: [[336,60]], mpu2: [[300,250],[336,280]]});
-          $(function () {
+          if (FT.ads.config('slotsInPage') === false) {
+            $(function () {
+              FT.ads.gpt.init();
+            });
+          } else {
             FT.ads.gpt.init();
-          });
+          }
       }
     }
 
