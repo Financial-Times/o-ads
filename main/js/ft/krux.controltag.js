9899d8afaa72ba7c9f2e0f879d3197c269167d68
Andrew Georgiou <andrew.georgiou@ft.com>
2013-08-27 14:33:44 +0100

    US33803 - Krux control tag

---
+++ main/js/ft/krux.controltag.js
@@ -0,0 +1,16 @@
+if(!window.FT) { window.FT = {};}
+if(!FT.ads) {
+    /**
+    * @namespace FT.ads
+    */
+FT.ads={};
+FT.ads.kruxConfigId="IhGt1gAD";
+var Krux = window.Krux||((Krux=function(){Krux.q.push(arguments);}).q=[]);
+  (function(){
+    var k=document.createElement('script');k.type='text/javascript';k.async=true;
+    var m,src=(m=location.href.match(/\bkxsrc=([^&]+)/))&&decodeURIComponent(m[1]);
+    k.src = /^https?:\/\/([^\/]+\.)?krxd\.net(:\d{1,5})?\//i.test(src) ? src : src === "disable" ? "" :
+      (location.protocol==="https:"?"https:":"http:")+"//cdn.krxd.net/controltag?confid="+FT.ads.kruxConfigId;
+    var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(k,s);
+  }());
+}
