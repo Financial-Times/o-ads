b932c4494b584a1cc29a7877aef35ddf85ca2a77
Robin Marr <robin.marr@ft.com>
2013-08-28 17:24:48 +0100

    US33803: Fix IE testing failure
    Krux integration work

--- main/js/ft/krux.controltag.js
+++ main/js/ft/krux.controltag.js
@@ -4,11 +4,13 @@ if(!FT.ads) { FT.ads ={};}
     * @namespace FT.ads
     */
 FT.ads.kruxConfigId= (FT.ads.getDFPSite().match(/^ft.com/)) ? "IgnVxTJW" : "IhGt1gAD";
-var Krux = window.Krux||((Krux=function(){Krux.q.push(arguments);}).q=[]);
-  (function(){
-    var k=document.createElement('script');k.type='text/javascript';k.async=true;
-    var m,src=(m=location.href.match(/\bkxsrc=([^&]+)/))&&decodeURIComponent(m[1]);
-    k.src = /^https?:\/\/([^\/]+\.)?krxd\.net(:\d{1,5})?\//i.test(src) ? src : src === "disable" ? "" :
-      (location.protocol==="https:"?"https:":"http:")+"//cdn.krxd.net/controltag?confid="+FT.ads.kruxConfigId;
-    var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(k,s);
-  }());
+if (!window.Krux) {
+  ((Krux=function(){Krux.q.push(arguments);}).q=[]);
+}
+(function(){
+  var k=document.createElement('script');k.type='text/javascript';k.async=true;
+  var m,src=(m=location.href.match(/\bkxsrc=([^&]+)/))&&decodeURIComponent(m[1]);
+  k.src = /^https?:\/\/([^\/]+\.)?krxd\.net(:\d{1,5})?\//i.test(src) ? src : src === "disable" ? "" :
+    (location.protocol==="https:"?"https:":"http:")+"//cdn.krxd.net/controltag?confid="+FT.ads.kruxConfigId;
+  var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(k,s);
+}());
