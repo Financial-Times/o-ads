9007a1b40c3cfeda1946b9d6ac23a44f7baab616
Robin Marr <robin.marr@ft.com>
2013-10-04 10:27:33 +0100

    US31899: Move config tests to own file to help test stability on the grid
    Serve ads using GPT on HTSI

--- test/js/third.party.gpt.test.js
+++ test/js/third.party.gpt.test.js
@@ -74,26 +74,26 @@
         test('Attach GPT library to page', function () {
             expect(2);
 
-            // initial number of async
-            var initialScripts = $('script').size();
-
-            FT.ads.gpt.attach();
-            deepEqual($('script').size() - initialScripts, 1, 'a new script tag has been added to the page.' );
-
             // wait for a maximum of 5 seconds for the google code to load
             // the display methods is tested to see if the lib is available
             QUnit.stop();
 
+            // initial number of async
+            var initialScripts = $('script').size();
+
+            FT.ads.gpt.attach();
             var totalTime = 0,
                 maxTime = 5000,
                 interval = 100,
                 timer = setInterval(function () {
                     totalTime += interval;
                     if (!!googletag.display) {
+                        deepEqual($('script').size() - initialScripts, 1, 'a new script tag has been added to the page.' );
                         ok(true, 'GPT available after ' + totalTime / 1000 + ' seconds');
                         clearInterval(timer);
                         QUnit.start();
                     } else if (interval === maxTime) {
+                        deepEqual($('script').size() - initialScripts, 1, 'a new script tag has been added to the page.' );
                         ok(false, 'GPT was not available after ' + maxTime / 1000 + ' seconds');
                         clearInterval(timer);
                         QUnit.start();
