40e92711b216abd3383a0e4a55428da907aa470f
Andrew Georgiou <andrew.georgiou@ft.com>
2014-01-15 19:35:22 +0000

    DE11013: debugging grid issue
    CSS issues with right hand rail and Grid build issues

--- test/js/third.party.switcher.test.js
+++ test/js/third.party.switcher.test.js
@@ -4,51 +4,47 @@
     function runTests() {
 
         module('Third party switcher', {
-            setup: function () {
+           setup: function () {
                 window.iframe = $('<iframe>').appendTo('body');
-            },
-            teardown: function () {
-                //win.iframe.remove();
             }
+
         });
-
-        test( "global switch", function() {
-          QUnit.stop();
+        test('global switch', function () {
+        QUnit.stop();
           iframe.load(function () {
-                // Use the iframe context for our assertions
-                var win = this.contentWindow,
-                    FT = win.FT,
-                    TEST = win.TEST;
-
+            var win = this.contentWindow,
+                FT = win.FT,
+                TEST = win.TEST;
                 deepEqual(FT.ads.config('ftads:mode'), 'gpt', 'Library is configured for GPT');
                 ok(TEST.sinon.adCallSpy.called, 'adCall method is still called (for backwards compatability)');
-                ok(!TEST.sinon.getTagSpy.called, 'getTag method is no longer called');
+                            ok(!TEST.sinon.getTagSpy.called, 'getTag method is no longer called');
                 ok(!TEST.sinon.getAudSciSpy.called, 'getAudSci method is no longer called');
-            QUnit.start();
-          });
+         QUnit.start();
+        });
+
           iframe.attr('src', '../iframes/third.party.switcher.global.html');
         });
 
-        test( "meta switch", function() {
-            QUnit.stop();
-            iframe.load(function () {
-                // Use the iframe context for our assertions
-                var win = this.contentWindow,
-                    FT = win.FT;
-                    TEST = win.TEST;
 
+        test('meta switch', function () {
+        QUnit.stop();
+          iframe.load(function () {
+            var win = this.contentWindow,
+                FT = win.FT,
+                TEST = win.TEST;
                 deepEqual(win.$('meta[name="ftads:mode"]').attr('content'), 'gpt', 'the meta configuration is present');
                 deepEqual(FT.ads.config('ftads:mode'), 'gpt', 'Library is configured for GPT');
                 ok(TEST.sinon.adCallSpy.called, 'adCall method is called (for backwards compatability)');
                 ok(!TEST.sinon.getTagSpy.called, 'getTag method is no longer called');
                 ok(!TEST.sinon.getAudSciSpy.called, 'getAudSci method is no longer called');
-                QUnit.start();
-            });
-            iframe.attr('src', '../iframes/third.party.switcher.meta.html');
+         QUnit.start();
         });
 
-        test( "switch off", function() {
-            QUnit.stop();
+                  iframe.attr('src', '../iframes/third.party.switcher.meta.html');
+        });
+
+        test('switch off', function () {
+        QUnit.stop();
             iframe.load(function () {
                 // Use the iframe context for our assertions
                 var win = this.contentWindow,
@@ -64,7 +60,7 @@
             });
             iframe.attr('src', '../iframes/third.party.switcher.off.html');
         });
-    }
+}
 
     $(runTests);
 }(window, document, jQuery));
