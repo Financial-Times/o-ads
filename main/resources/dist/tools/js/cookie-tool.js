51c8e9a87c1082cb812c13b45c013538689e1a15
Andrew Georgiou <andrew.georgiou@ft.com>
2014-04-23 16:57:15 +0100

    DE11792: cleaning up pull request
    Cookie tool does not work in IE. Despite setting via buttons, this has no effect.

--- main/resources/dist/tools/js/cookie-tool.js
+++ main/resources/dist/tools/js/cookie-tool.js
@@ -1,3 +1,26 @@
+//polyfill forEach for IE8 
+if (!Array.prototype.forEach)
+{
+  Array.prototype.forEach = function(fun /*, thisArg */)
+  {
+    "use strict";
+
+    if (this === void 0 || this === null)
+      throw new TypeError();
+
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun !== "function")
+      throw new TypeError();
+
+    var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+    for (var i = 0; i < len; i++)
+    {
+      if (i in t)
+        fun.call(thisArg, t[i], i, t);
+    }
+  };
+}
 function encode(str) {
   return encodeURIComponent(str);
 }
@@ -42,7 +65,7 @@ $(function(){
   function renderMessage(msg, type) {
     type = type ? ' ' + type : '';
     $('.alert-message').remove();
-    $('<p class="alert-message' + type + '"></p>').append(msg).insertAfter(header);
+   $('<p class="alert-message' + type + '"></p>').append(msg).insertAfter(header);
     return true;
   }
 
@@ -70,8 +93,7 @@ $(function(){
     return false;
   });
 
-  form.find('select[name="domain"]')
-    .append('<option>' + document.location.hostname + '</option>');
+  form.find('select[name="domain"]')   .append('<option>' + document.location.hostname + '</option>');
 
   $('#cookies').on('click', '.delete', function(ev){
     var row = $(this).parents('tr'),
@@ -167,8 +189,8 @@ $(function(){
           renderMessage('sorry, failed to remove cookie: name ' + hash.name, 'error');
         }
       },
-      default: function (){
-        renderMessage('ya\'ll dont be fiddlin wit ma cookie tool!!');
+      'default': function (){
+        renderMessage('yall dont be fiddlin wit ma cookie tool!!');
       }
     }
 
@@ -191,7 +213,7 @@ $(function(){
 
     for(name in hashes.cookies){
       value = hashes.cookies[name];
-      table.append('<tr><td><span class="delete" data-cookie-name="' + name + '">&times;</span><td>' + decode(name) +'</td><td>'+ decode(value) + '</td></tr>');
+      table.append('<tr><td><span class="delete" data-cookie-name="' + name + '">&times;</span></td><td>' + decode(name) +'</td><td>'+ decode(value) + '</td></tr>');
     }
   }
 });
