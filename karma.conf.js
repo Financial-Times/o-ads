fd85961804bb1fe9c21d3784ce57c7a01e30a3c4
Robin Marr <robin.marr@ft.com>
2014-10-20 16:26:53 +0100

    Fix some syntx errors, get all test dependencies from npm or bower

--- karma.conf.js
+++ karma.conf.js
@@ -2,27 +2,15 @@ module.exports = function(config) {
   config.set({
     basePath: '',
     autoWatch: true,
-    frameworks: ['qunit', 'browserify'],
+    frameworks: ['qunit', 'sinon', 'browserify'],
     files: [
       'test/js/util/initialize.namespace.js',
-      'test/lib/jquery/*.js',
-      'test/lib/sinon/*.js',
+      'bower_components/jquery-1.7.2.min/index.js',
+      'bower_components/jquery.cookie/index.js',
       'test/js/util/test.helpers.js',
       'test/js/util/gpt.js',
       'test/js/util/*.js',
-     // 'test/js/advertising.utils.cookie.test.js',
-     'test/js/advertising.utils.cookie.test.js',
-     'test/js/advertising.utils.responsive.test.js',
-      'test/js/advertising.utils.test.js',
-      'test/js/advertising.utils.timers.test.js',
-      'test/js/chartbeat.test.js',
-     'test/js/krux.test.js',
-     'test/js/metadata.test.js',
-      'test/js/third.party.config.test.js',
-      'test/js/third.party.gpt.test.js',
-      'test/js/third.party.slots.test.js',
-     'test/js/third.party.targeting.test.js'
-
+      'test/js/*.test.js'
     ],
     browsers: ['Chrome'],
 
