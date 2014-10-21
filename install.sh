633e1fc32be452627af4780f149bcb3bfa615527
Andrew Georgiou <andrew.georgiou@ft.com>
2014-10-21 11:37:58 +0100

    removing install shell script as this is not necessary, we can use the package.json for this purpose

--- install.sh
+++
@@ -1,6 +0,0 @@
-npm install gulp-tag-version --save-dev
-npm install gulp-git --save-dev
-npm install gulp-util --save-dev
-npm install map-stream --save-dev
-npm install gulp-bump --save-dev
-npm install gulp-filter --save-dev
