0fb07c5021e81ed75d46ffaafc1ca1049f453fa8
Robin Marr <robin.marr@ft.com>
2014-11-18 17:17:55 +0000

    Test files

---
+++ test/js/util/rubicon-valuation-mock.js
@@ -0,0 +1,9 @@
+window.RubiconInsight = function RubiconInsight(){
+};
+
+var stubs = RubiconInsight.sinon = sinon.sandbox.create();
+
+RubiconInsight.init = stubs.stub();
+
+RubiconInsight.start = stubs.stub();
+
