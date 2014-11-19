63ac678b55d299270e7f17dc7eda50fe7764dcb0
April Mateo <april.mateo@ft.com>
2014-11-19 08:11:30 +0000

    added unit tests for rubicon

--- test/js/rubicon.test.js
+++ test/js/rubicon.test.js
@@ -25,9 +25,34 @@
    });
 
    test('decorate slots.initSlot', function () {
+      TEST.beginNewPage({config: {rubicon: {id: 10232, site: 26290}}});
+
       FT.ads.rubicon.decorateInitSlot();
 
       equal(FT.ads.slots.initSlot, FT.ads.rubicon.initValuation, 'ads.slots.initSlot is decorated by ads.rubicon.initValuation');
+   });
+
+   test('test rubicon insight - no rubicon mapping for given site and size', function () {
+      var initValuation = sinon.spy(FT.ads.rubicon, 'initValuation');
+      TEST.beginNewPage({config: {dfp_site: 'test.5887.home', 'dfp_zone': ' index', rubicon: {id: 10232, site: 26290,
+         mappings: {
+         'test.5887.home': {
+            banlbGPT: {
+               size: '728x90',
+               zone: 123456
+            }}}}}});
+
+      FT.ads.rubicon.init(FT.ads);
+      FT.ads.slots.initSlot("mpu");
+
+      ok(initValuation.called, 'ads.rubicon.initValuation called');
+      ok(rubiconInsight.init.notCalled, 'rubiconInsight.init is not called');
+      ok(rubiconInsight.start.notCalled, 'rubiconInsight.start is not Called');
+      ok(rubiconInsight.onValuationLoaded.notCalled, 'rubiconInsight.onValuationLoaded is not Called');
+   });
+
+   test('test rubicon insight - ok', function () {
+
    });
 
    $(runTests);
