555cead052b764840d285e8c2c9218605d3c2a3f
Andrew Georgiou <andrew.georgiou@ft.com>
2015-03-04 13:42:10 +0000

    ADS-185: lazy loading, disable for ios

--- src/js/slots.js
+++ src/js/slots.js
@@ -52,7 +52,7 @@ function iOSversion() {
     return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
   }
 }
-ver = iOSversion();
+var ver = iOSversion();
 if (ver[0] >= 8) {
     if (window.addEventListener) {
         window.addEventListener('DOMContentLoaded', handler, false);
