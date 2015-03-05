bc4e105ab0c60722206c31841d0677f90ebbee5a
Andrew Georgiou <andrew.georgiou@ft.com>
2015-03-05 11:15:04 +0000

    ADS-185: Moving ios detection to utils now that it has been confirmed to be working correctly

--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -452,7 +452,7 @@ module.exports.nodeListToArray = function(obj) {
   return array;
 };
 
-module.exports.getiOSversion {
+module.exports.getiOSversion = function() {
   if (/iP(hone|od|ad)/.test(navigator.platform)) {
     var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
     return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
