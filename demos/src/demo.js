9ec8bacb26630b060cdfcc3fa0e2b164e296e21b
Andrew Georgiou <andrew.georgiou@ft.com>
2015-03-16 10:24:00 +0000

    ADS-212: Updating Demos

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -2,17 +2,4 @@
 var ads = require('../../main.js').init({
     dfp_site: 'test.5887.origami', 
     eid: 'oads', 
-    formats: {
-       leaderboard: {
-         sizes: [[970,90]]
-       },
-       leaderboardOop: {
-          sizes: [[970,90]],
-          outOfPage: true
-       }
-    }
 });
-
-ads.slots.initSlot('leaderboard');
-ads.slots.initSlot('leaderboardOop');
-
