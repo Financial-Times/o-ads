c99cb997f1f337404aa9384c410fafe57342b637
Andrew Georgiou <andrew.georgiou@ft.com>
2015-04-30 12:09:08 +0100

    ADS-323: GPT video URL

--- src/js/video.js
+++ src/js/video.js
@@ -9,52 +9,54 @@
  * @memberof FT.ads
 */
 "use strict";
-var config = require('./config');
-var targeting = require('./targeting');
-
-function gptVideoURL(pos, vkv){
-    var gpt = config('gpt');
-    var URL, additionalAdTargetingParams, fullURL;
-    var buildCustomParams = function (vkv) {
-        var allTargeting = targeting.get();
-        var results = '',
-        kruxSegs = allTargeting.ksg,
-        includeParams = [ 'playlistid', 'playerid', '07', 'ksg', '06', 'slv', 'eid', '05', '19', '21', '27', '20', '02', '14', 'cn', '01','rfrsh', 'dcopt', 'brand', 'section', 'lnID', 'specialBadging', 'kuid'];
-        var i;
-        for (i=0;i<includeParams.length;i++){
-            var key= includeParams[i];
-            var value = false;
-            if (typeof allTargeting[key] !== 'undefined') {
-                value = allTargeting[key];
-            } else if (typeof vkv !== 'undefined' && (typeof vkv[key] !== 'undefined')) {
-                value = vkv[key];
-            }
-            if (key === 'ksg' && kruxSegs) {
-                value=kruxSegs.slice(0,config('kruxMaxSegs')).join(',');
-            }
-            results += !value ? '' : key + '=' + value + '&';
-        }
-        return results;
-    };
-    var encodeCustParams = function (vkv) {
-        return encodeURIComponent(buildCustomParams(vkv));
-    };
-    URL = "http://pubads.g.doubleclick.net/gampad/ads?env=vp&gdfp_req=1&impl=s&output=xml_vast2&iu=/" + gpt.network + "/" + gpt.site + "/" + gpt.zone + "&sz=592x333|400x225&unviewed_position_start=1&scp=pos%3D" + pos;
-    additionalAdTargetingParams = encodeCustParams(vkv);
-    fullURL = (buildCustomParams(vkv) === "") ? URL : URL + '&' + buildCustomParams(vkv);
-
-    return {
-        urlStem : URL,
-        additionalAdTargetingParams : additionalAdTargetingParams,
-        fullURL : fullURL
-    };
-}
+var ads;
 
 function buildURLForVideo(zone, pos, vidKV){
-    var gpt = config('gpt');
     pos = pos || 'video';
     vidKV = vidKV || {};
-        return gptVideoURL(pos, vidKV);
+
+    var gptVideoURL = function(){
+        var URL, additionalAdTargetingParams, fullURL;
+        var buildCustomParams = function (vkv) {
+            var allTargeting = ads.targeting.get();
+                        var results = '',
+                        kruxSegs = allTargeting.ksg,
+                        includeParams = [ 'playlistid', 'playerid', '07', 'ksg', 'kuid', 'khost', '06', 'slv', 'eid', '05', '19', '21', '27', '20', '02', '14', 'cn', '01','rfrsh', 'dcopt', 'brand', 'section', 'lnID', 'specialBadging'];
+                        var i;
+                        for (i=0;i<includeParams.length;i++){
+                            var key= includeParams[i];
+                            var value = false;
+                            if (typeof allTargeting[key] !== 'undefined') {
+                                value = allTargeting[key];
+                            } else if (typeof vkv !== 'undefined' && (typeof vkv[key] !== 'undefined')) {
+                                value = vkv[key];
+                            }
+                            if (key === 'ksg' && kruxSegs) {
+                                value=kruxSegs.slice(0,ads.config('kruxMaxSegs')).join(',');
+                            }
+                            results += !value ? '' : key + '=' + value + '&';
+                        }
+                        return results;
+        };
+        var encodeCustParams = function (vkv) {
+            return encodeURIComponent(buildCustomParams(vkv));
+        };
+
+        URL = "http://pubads.g.doubleclick.net/gampad/ads?env=vp&gdfp_req=1&impl=s&output=xml_vast2&iu=/5887/"+ ads.config('dfp_site') + "/" + ads.config('dfp_zone') + "&sz=592x333|400x225&unviewed_position_start=1&scp=pos%3D" + pos;
+        additionalAdTargetingParams = encodeCustParams(vidKV);
+        fullURL = (buildCustomParams(vidKV) === "") ? URL : URL + '&' + buildCustomParams(vidKV);
+
+        return {
+            urlStem : URL,
+            additionalAdTargetingParams : additionalAdTargetingParams,
+            fullURL : fullURL
+        };
+    };
+        return gptVideoURL();
 }
+
+buildURLForVideo.init = function (impl) {
+    ads = impl;
+};
 
 module.exports = buildURLForVideo;
