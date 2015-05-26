a3ea4b62404d64503e8b36e2290edef8531d18db
helderroem <helderroem@users.noreply.github.com>
2015-05-26 14:09:34 +0100

    Fixes for ft.com clean up

--- src/js/data-providers/chartbeat.js
+++ src/js/data-providers/chartbeat.js
@@ -9,7 +9,7 @@
 * @name targeting
 * @memberof FT.ads
 */
-"use strict";
+'use strict';
 var utils = require('../utils');
 
 /**
@@ -19,15 +19,16 @@ var utils = require('../utils');
 * @memberof ChartBeat
 * @lends ChartBeat
 */
-module.exports.init = function () {
-	utils.on('ready', function(event){
+module.exports.init = function() {
+	utils.on('ready', function(event) {
 		var slot = event.detail.slot;
-		if(slot.chartbeat !== false){
-			slot.container.setAttribute('data-cb-ad-id', slot.name);
+		if (slot.chartbeat) {
+			var name = utils.isNonEmptyString(slot.chartbeat) ? slot.chartbeat : slot.name;
+			slot.container.setAttribute('data-cb-ad-id', name);
 		}
 	});
 
-	utils.on('refresh', function(event){
+	utils.on('refresh', function(event) {
 		if (window.pSUPERFLY) {
 			window.pSUPERFLY.refreshAd(event.detail.name);
 		}
