c3ba5223c4337a668e1139a2160d64a967b7d24a
Andrew Georgiou <andrew.georgiou@ft.com>
2015-06-03 11:58:03 +0100

    ADS-260 Chartbeat on Next

--- src/js/data-providers/chartbeat.js
+++ src/js/data-providers/chartbeat.js
@@ -11,6 +11,7 @@
 */
 'use strict';
 var utils = require('../utils');
+var config = require('../config');
 
 /**
 * initialise chartbeat functionality
@@ -19,18 +20,39 @@ var utils = require('../utils');
 * @memberof ChartBeat
 * @lends ChartBeat
 */
-module.exports.init = function() {
-	utils.on('ready', function(event) {
-		var slot = event.detail.slot;
-		if (slot.chartbeat) {
-			var name = utils.isNonEmptyString(slot.chartbeat) ? slot.chartbeat : slot.name;
-			slot.container.setAttribute('data-cb-ad-id', name);
-		}
-	});
 
-	utils.on('refresh', function(event) {
-		if (window.pSUPERFLY) {
-			window.pSUPERFLY.refreshAd(event.detail.name);
-		}
-	});
+
+module.exports.init = function() {
+	var gpt = config('gpt') || {};
+	this.config = config('rubicon') || {};
+	if (config.uid && config.domain) {
+
+	//CONFIGURATION
+		var _sf_async_config = {
+			uid: this.config.uid,
+			domain: this.config.domain,
+			useCanonical: true,
+			zone:gpt.site + "/" + gpt.zone
+		};
+
+	// LOAD LIBRARY
+		window._sf_endpt=(new Date()).getTime();
+		utils.attach('//static.chartbeat.com/js/chartbeat_pub.js', true);
+
+	// ADD CB DATA-ATTRIBUTE TO CONTAINING DIV
+		utils.on('ready', function(event) {
+			var slot = event.detail.slot;
+			if (slot.chartbeat) {
+				var name = utils.isNonEmptyString(slot.chartbeat) ? slot.chartbeat : slot.name;
+				slot.container.setAttribute('data-cb-ad-id', name);
+			}
+		});
+
+		utils.on('refresh', function(event) {
+			if (window.pSUPERFLY) {
+				window.pSUPERFLY.refreshAd(event.detail.name);
+			}
+		});
+
+	}
 };
