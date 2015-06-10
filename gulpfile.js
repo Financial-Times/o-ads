6a4dd8c97a250b3a9261f21815e9d8279eaf683d
Robin Marr <robin.marr@ft.com>
2015-06-10 16:36:20 +0100

    cleanup gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,48 +4,10 @@
 printAscii();
 var path = require('path');
 var gulp = require('gulp');
-var obt = require('origami-build-tools');
 var git = require('gulp-git');
 var bump = require('gulp-bump');
 var filter = require('gulp-filter');
-var plato = require('gulp-plato');
 var tagVersion = require('gulp-tag-version');
-var ssg = require('gulp-ssg');
-var rename = require('gulp-rename');
-var data = require('gulp-data');
-var matter = require('gray-matter');
-var markdown = require('gulp-markdown');
-var wrap = require('gulp-wrap');
-var sequence = require('run-sequence');
-var clean = require('rimraf');
-
-var config = {
-	origami: require('./origami.json'),
-	bower: require('./bower.json'),
-	docs: {
-		filters: {
-			js: filter('**/*.js'),
-			md: filter('**/*.md')
-		},
-		templates: {
-			main: path.join(__dirname, './docs/templates/o-tech.html'),
-			js: path.join(__dirname, './docs/templates/src.html')
-		}
-	},
-	reports: {
-		plato: {
-			dest: './build/reports/plato',
-			options: {
-				jshint: {
-					options: require('./node_modules/origami-build-tools/config/jshint.json')
-				},
-				complexity: {
-					trycatch: true
-				}
-			}
-		}
-	}
-};
 
 /**
  * Bumping version number and tagging the repository with it.
@@ -88,84 +50,6 @@ function inc(type, callback) {
 gulp.task('release:patch', function(callback) { inc('patch', callback); });
 gulp.task('release:minor', function(callback) { inc('minor', callback); });
 gulp.task('release:major', function(callback) { inc('major', callback); });
-
-gulp.task('report:plato', function() {
-	var conf = config.reports.plato;
-	gulp.src(['./main.js', './src/js/**/*.js'])
-		.pipe(plato(conf.dest, conf.options));
-});
-
-gulp.task('build', function() {
-	obt.build(gulp, {
-		sourcemaps: true
-	});
-});
-
-gulp.task('report:qunit', function() {
-	obt.build(gulp, {
-		files: ['test/qunit/setup.js', 'test/qunit/*.test.js'],
-		sourcemaps: true
-	});
-});
-
-gulp.task('reports', ['build', 'report:plato']);
-
-gulp.task('docs:build', function() {
-	var filters = config.docs.filters;
-	var templates = config.docs.templates;
-	return gulp.src(['./docs/**/*.md', './src/js/**/*.js'])
-		.pipe(filters.md)
-			.pipe(data(function(file) {
-				var m = matter(String(file.contents));
-				file.matter = matter.stringify('', m.data);
-				file.contents = new Buffer(m.content);
-				return m.data;
-			}))
-			.pipe(markdown())
-		.pipe(filters.md.restore())
-		.pipe(filters.js)
-			.pipe(data(function(file) {
-				var data = {
-					title: path.parse(file.path).name
-				};
-				file.matter = matter.stringify('', data);
-				return data;
-			}))
-			.pipe(wrap({ src: templates.js }, {}, { engine: 'hogan' }))
-			.pipe(rename(function(file) {
-				file.dirname = path.join('developer-guide/src/', file.dirname);
-			}))
-		.pipe(filters.js.restore())
-		.pipe(wrap('{{file.matter}}{{{contents}}}', {}, { engine: 'hogan' }))
-		.pipe(rename({ extname: '.html' }))
-		.pipe(gulp.dest('./build/site/base'));
-});
-
-gulp.task('docs:gh', function() {
-	var templates = config.docs.templates;
-
-	return gulp.src('./build/site/base/**/*.html')
-		.pipe(data(function(file) {
-			var m = matter(String(file.contents));
-			file.contents = new Buffer(m.content);
-			return m.data;
-		}))
-		.pipe(ssg())
-		.pipe(wrap(
-			{ src: templates.main },
-			{ siteTitle: config.origami.description},
-			{ engine: 'hogan' }
-		))
-		.pipe(gulp.dest('./build/site/gh'));
-});
-
-gulp.task('clean:docs', function(cb) {
-	clean('build/site', cb);
-});
-
-gulp.task('docs', function() {
-	sequence('clean:docs', 'docs:build', 'docs:gh');
-});
 
 function printAscii() {
 	console.log('                                  __           ');
