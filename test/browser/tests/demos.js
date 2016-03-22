a1651e4a3f158be5d1f6f0ac534cad90b722c4a2
Vlad Dubrovskis <vlad.dubrovskis@ft.com>
2016-03-22 12:11:31 +0000

    remove a looong wait statement

--- test/browser/tests/demos.js
+++ test/browser/tests/demos.js
@@ -14,7 +14,6 @@ module.exports = function (browser) {
 					// switch focus to first iframe
 					.frame(0)
 					// wait for 5 seconds for advert to appear
-					.wait('1000000')
 					.waitForElementPresent("img", 5000, 'Advert image is visible')
 					// make sure we can see the correct URL
 					.assert.attributeContains('img', 'src', 'https://tpc.googlesyndication.com/simgad/1809750796305150566', 'Correct image is displayed for leaderboard')
