f0d69071d3d631fac03850dbc733b7712da50d59
Vlad Dubrovskis <vlad.dubrovskis@ft.com>
2016-03-22 11:40:24 +0000

    add basic test

---
+++ test/browser/tests/demos.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = function (browser) {
+
+		'goToDemoPage': function () {
+			return browser
+				.url(browser.launch_url + "demos/local/Individual-Ad.html")
+				.waitForElementVisible("body", 5000)
+				.assert.title("o-ads: Individual-Ad demo", 'Page title is correct')
+		},
+		'verifyIndividualAdIsVisible': function () {
+			return browser
+				.assert.visible("#leaderboard-gpt", 'Advert has been initiated and visible')
+					// switch focus to first iframe
+					.frame(0)
+					// wait for 5 seconds for advert to appear
+					.waitForElementPresent("img", 5000, 'Advert image is visible')
+					// make sure we can see the correct URL
+					.assert.attributeContains('img', 'src', 'https://tpc.googlesyndication.com/simgad/1809750796305150566', 'Correct image is displayed for leaderboard')
+					// switch focus back to main page
+          .frame(null)
+		},
+
+		after: function (browser) {
+			// end test
+			browser.end();
+		}
+};
