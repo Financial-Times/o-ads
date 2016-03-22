0d53be01374e6cc449a34e5b642e50634ba5b1ef
Vlad Dubrovskis <vlad.dubrovskis@ft.com>
2016-03-22 12:00:11 +0000

    fix browser path for first test

--- test/browser/tests/demos.js
+++ test/browser/tests/demos.js
@@ -4,7 +4,7 @@ module.exports = function (browser) {
 
 		'goToDemoPage': function () {
 			return browser
-				.url(browser.launch_url + "demos/local/Individual-Ad.html")
+				.url(browser.launch_url + "/Individual-Ad.html")
 				.waitForElementVisible("body", 5000)
 				.assert.title("o-ads: Individual-Ad demo", 'Page title is correct')
 		},
@@ -14,6 +14,7 @@ module.exports = function (browser) {
 					// switch focus to first iframe
 					.frame(0)
 					// wait for 5 seconds for advert to appear
+					.wait('1000000')
 					.waitForElementPresent("img", 5000, 'Advert image is visible')
 					// make sure we can see the correct URL
 					.assert.attributeContains('img', 'src', 'https://tpc.googlesyndication.com/simgad/1809750796305150566', 'Correct image is displayed for leaderboard')
