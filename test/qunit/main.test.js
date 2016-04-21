8a0e60d465d4f50ca478ed48fd2053ff77652122
Simon Legg <leggsimon@gmail.com>
2016-04-21 15:12:34 +0100

    Add unit tests

--- test/qunit/main.test.js
+++ test/qunit/main.test.js
@@ -54,3 +54,28 @@ QUnit.test("debug sets and unsets oAds in local storage if it wasn't set", funct
 	assert.ok(admantxDebug.called, 'Admantx debug function is called');
 	assert.notOk(localStorage.getItem('oAds'), 'oAds value in local storage was removed');
 });
+
+QUnit.test('destroy removes scripts added to DOM', function(assert) {
+	this.ads.utils.attach.restore();
+
+	const initialScripts = document.querySelectorAll('script').length;
+	const tag = this.ads.utils.attach(this.nullUrl);
+
+	assert.equal(document.querySelectorAll('script').length, initialScripts + 1, 'a new script tag has been added to the page.');
+	assert.equal(document.querySelectorAll('[o-ads]').length, 1, 'the script tag has an o-ads attribute');
+
+	this.ads.destroy()
+	assert.equal(document.querySelectorAll('script').length, initialScripts, 'the script has been removed from the page.');
+	assert.equal(document.querySelectorAll('[o-ads]').length, 0, 'there are no script tag with an o-ads attribute');
+});
+
+QUnit.test('destroy removes ad slots', function(assert) {
+	const node = this.fixturesContainer.add('<div data-o-ads-name="test" data-o-ads-formats="MediumRectangle"></div>');
+	this.ads.slots.initSlot(node);
+	assert.equal(document.querySelectorAll('.o-ads__outer').length, 1, 'an ad slot was created');
+
+	this.ads.destroy()
+	assert.equal(document.querySelectorAll('.o-ads__outer').length, 0, 'the ad slot was destroyed');
+});
+
+
