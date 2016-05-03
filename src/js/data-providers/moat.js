6bf8ab19a87722df50656b159d532f5421145132
Simon Legg <leggsimon@gmail.com>
2016-05-03 17:03:22 +0100

    Improve script loaded logic

--- src/js/data-providers/moat.js
+++ src/js/data-providers/moat.js
@@ -7,14 +7,15 @@ function Moat() {
 }
 
 Moat.prototype.init = function() {
-	this.config = config('moat');
+    this.config = config('moat');
 
-	/* istanbul ignore else	*/
-	if (this.config && this.config.id && !utils.isScriptAlreadyLoaded(src)) {
-		var src = "https://z.moatads.com/" + this.config.id + "/moatcontent.js";
-		utils.attach(src, true);
-	}
-
+    /* istanbul ignore else    */
+    if (this.config && this.config.id) {
+        var src = "https://z.moatads.com/" + this.config.id + "/moatcontent.js";
+        if (!utils.isScriptAlreadyLoaded(src)) {
+            utils.attach(src, true);
+        }
+    }
 };
 
 module.exports = new Moat();
