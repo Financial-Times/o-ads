0cda64dc67d837351cf2f944979655be17e97da1
Vlad Dubrovskis <vlad.dubrovskis@ft.com>
2016-07-04 16:37:43 +0100

    update the way we scroll in the test

--- test/browser/basic/individual-ad-lazy-load.js
+++ test/browser/basic/individual-ad-lazy-load.js
@@ -14,7 +14,7 @@ module.exports = {
 	'Step 2: verify the leaderboard advert is displayed': function (browser) {
 		browser
 			.assert.hidden('#leaderboard-gpt', 'Advert has not been initiated and is not visible')
-				.execute('scrollTo(0,10000)')
+				.execute(function() { window.scrollBy(0, 10000); }, [])
 				.pause(wait / 2)
 				.assert.visible('#leaderboard-gpt', 'Advert has been initiated and visible')
 				// switch focus to first iframe
