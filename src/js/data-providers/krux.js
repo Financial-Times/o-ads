524bf1a15753bef6effc545a9816f4a51c7ff359
Arjun Gadhia <arjungadhia@gmail.com>
2016-08-31 10:38:24 +0100

    Update KRux URL to newer one

--- src/js/data-providers/krux.js
+++ src/js/data-providers/krux.js
@@ -37,7 +37,7 @@ Krux.prototype.init = function() {
 		if (m) {
 			src = decodeURIComponent(m[1]);
 		}
-		const finalSrc = /^https?:\/\/([^\/]+\.)?krxd\.net(:\d{1,5})?\//i.test(src) ? src : src === "disable" ? "" : `//cdn.krxd.net/controltag?confid=${this.config.id}`;
+		const finalSrc = /^https?:\/\/([^\/]+\.)?krxd\.net(:\d{1,5})?\//i.test(src) ? src : src === "disable" ? "" : `//cdn.krxd.net/controltag/${this.config.id}.js`;
 		
 		const loadKruxScript = () => {
 			utils.attach(finalSrc, true, () => {
