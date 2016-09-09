1d25af06dfa557aa562eacd1fb04d6970e95c035
Gabrielle von Koss <gabivk@gmail.com>
2016-09-09 15:58:53 +0100

    fix browser tests

--- test/browser/basic/individual-ad-with-custom-targeting-values.js
+++ test/browser/basic/individual-ad-with-custom-targeting-values.js
@@ -6,20 +6,20 @@ module.exports = {
 		browser.url(browser.launch_url + '/Individual-Ad-with-Custom-Targeting-Values.html');
 	},
 
-	'Step 1: go to leaderboard with custom targeting demo page': function (browser) {
+	'Step 1: go to Billboard with custom targeting demo page': function (browser) {
 		browser
 			.waitForElementVisible('body', wait)
 			.assert.title('o-ads: Individual-Ad-with-Custom-Targeting-Values demo', 'Page title is correct');
 	},
-	'Step 2: verify the leaderboard advert is displayed': function (browser) {
+	'Step 2: verify the Billboard advert is displayed': function (browser) {
 		browser
-			.assert.visible('#Billboard-gpt', 'Advert has been initiated and visible')
+			.assert.visible('#billboard-gpt', 'Advert has been initiated and visible')
 				// switch focus to first iframe
 				.frame(0)
 				// wait for 1 second for advert to appear
 				.waitForElementPresent('img', wait, 'Advert image is visible')
 				// make sure we can see the correct URL
-				.assert.attributeContains('img', 'src', 'https://tpc.googlesyndication.com/simgad/14891015459176126032')
+				.assert.attributeContains('img', 'src', 'https://tpc.googlesyndication.com/simgad/6935553857235402720')
 				// switch focus back to main page
 				.frame(null);
 	},
