b4aed6a43f7cfed3eab93520dfd4612a64dd5f82
Alex Florisca <alexflorisca@gmail.com>
2018-09-04 12:42:34 +0100

    Tidy up tests

--- README.md
+++ README.md
@@ -2,15 +2,33 @@
 
 This is an Origami module that enables display advertising from [Googles DFP Ad server](http://www.google.com/dfp), and provides customised demographic, behavioural (via [Krux](http://www.krux.com/)), and contextual (via [Admantx](http://admantx.com/)) targeting.
 
-### Requirements
+## Requirements
 For basic use, a DFP account with Google is required.  
 Each targeting/tracking supplier will require their own configuration and setup.
 
-### Demos
+## Demos
 Demos for all ads currently served across ft.com are available in the [Origami Registry](http://registry.origami.ft.com/components/o-ads).
 
-### [Documentation](https://financial-times.github.io/o-ads)
+## [Documentation](https://financial-times.github.io/o-ads)
 Includes detailed installation and set-up instructions, along with details about the module.
+
+## Developing
+
+### Install & Demos
+
+- To install: `obt install`.
+- To run the demos: `obt demo`.
+- To run a demo server: `npm run demo-server`
+
+### Tests
+
+The testing setup is unlike other origami components. There are unit tests which we run with karma, and these can be run with `npm run test-unit`
+
+*Browser tests*
+We also use Nightwatch and Browserstack to run cross browser tests. To run these:
+
+1. `npm run demo-server` in your terminal. This will compile and launch the demos on http://localhost:3002. *This is needed for the next step to work*
+2. `npm run test-browser` will run a local browserstack tunnel and run the tests
 
 ## Migration Guide
 
--- test/browser/config/nightwatch-local.js
+++ test/browser/config/nightwatch-local.js
@@ -1,24 +1,64 @@
 #!/usr/bin/env node
 
 const Nightwatch = require('nightwatch');
+const browserstack = require('browserstack-local');
+const { exec, spawn } = require('child_process');
 
-if(process.env.NODE_ENV === 'development') {
-	require('dotenv').load();
-}
-
-try {
-	process.mainModule.filename = "./node_modules/.bin/nightwatch"
+// In production, the BrowserStackLocal tunnel is loaded as a separate process
+// to speed up the build
+if(process.env.NODE_ENV === 'production') {
 	Nightwatch.cli(function(argv) {
 		Nightwatch.CliRunner(argv)
 		.setup(null, function(){
 			// Runs after parallel tests
-			console.log("Finished running nightwatch tests");
+			console.log("Finished running nightwatch tests.");
 		})
 		.runTests(function(){
 			// Runs after single test
-			console.log("Finished running single nightwatch test");
+			console.log("Finished running single nightwatch test.");
 		});
-});
+	});
+	return;
+}
+
+require('dotenv').load();
+let bs_local;
+
+try {
+	if(!process.env.BROWSERSTACK_KEY || !process.env.BROWSERSTACK_USER || !process.env.BROWSERSTACK_LOCAL) {
+		throw new Error('Browserstack environment variables missing. Please set the following, either in a .env file or export in your shell: \n BROWSERSTACK_NAME \n BROWSERSTACK_KEY \n BROWSERSTACK_LOCAL');
+	}
+	// If there are any previous instances of BrowserStackLocal, kill them first.
+	exec(`pkill BrowserStackLocal`, err => {
+		// No Browserstack to kill
+		if(err) return;
+		else {
+			console.log("Existing BrowserStackLocal instance found, killing it...");
+		}
+	})
+
+	process.mainModule.filename = "./node_modules/.bin/nightwatch"
+	
+	// Code to start browserstack local before start of test
+	console.log("Connecting BrowserStackLocal tunnel...");
+	Nightwatch.bs_local = bs_local = new browserstack.Local();
+	bs_local.start({'key': process.env.BROWSERSTACK_KEY }, function(error) {
+		if (error) throw error;
+		console.log('Connected to Browserstack. Now testing...');
+		Nightwatch.cli(function(argv) {
+			Nightwatch.CliRunner(argv)
+			.setup(null, function(){
+				// Runs after parallel tests
+				bs_local.stop(function(){});
+				console.log("Finished running nightwatch tests.");
+			})
+			.runTests(function(){
+				// Runs after single test
+				bs_local.stop(function(){});
+				console.log("Finished running single nightwatch test.");
+			});
+		});
+	});
 } catch (ex) {
   console.log('There was an error while starting the test runner:\n\n');
   process.stderr.write(ex.stack + '\n');
