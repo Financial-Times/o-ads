0fa3e5c2d77dae8cfda918f3948cb4d88e282b26
Alex Florisca <alexflorisca@gmail.com>
2018-10-08 18:10:23 -0500

    Working tests

--- cypress/tests/unit/main.test.js
+++ cypress/tests/unit/main.test.js
@@ -6,32 +6,73 @@ describe('Main', () => {
 		cy.clearCookie('FTConsent');
 	});
 
-	// NOT WORKING
 	it('oAds.ready event fires after o.DOMContentLoaded', () => {
 		document.body.insertAdjacentHTML('beforeend', '<div data-o-ads-name="banlb2" data-o-ads-formats="MediumRectangle"></div>');
-		
-		const ads = new Ads();
-
+		new Ads();
 		const oAdsReadySpy = cy.spy();
-
 		utils.on('ready', oAdsReadySpy);
 
+		/*
+			cy runs two iframes. One loads the test, the other loads
+			a locally running app (if set).
+
+			window.document (here, refers to the test container)
+			cy.document() refers to the app env
+		*/
 		document.dispatchEvent(new Event('o.DOMContentLoaded'));
 
-		cy.wait(10000);
-
-		expect(oAdsReadySpy.callCount).to.equal(1);
-
-
-		// // NOT WORKING
-		// document.body.addEventListener('oAds.ready', function(event) {
-		// 	console.log(event.detail.name);
-		// 	console.log('oh heeey');
-		// 	expect(event.detail.name).to.equal('banlb2', 'our test slot is requested');
-		// 	expect(event.detail.slot.sizes).to.deep.equal([[300, 250]], 'with the correct sizes');
-		// });
-		
+		// Wait for event loop to finish
+		cy.wait(0).then(() => {
+			const eventDetail = oAdsReadySpy.args[0][0].detail;
+			expect(oAdsReadySpy.callCount).to.equal(1);
+			expect(eventDetail.name).to.equal('banlb2');
+			expect(eventDetail.slot.sizes).to.deep.equal([[300, 250]]);
+		});
 	});
 
 
+	context('Initialising', () => {
+		it('oAds.initialised event is triggered', () => {
+			const ads = new Ads();
+			const oAdsInitSpy = cy.spy();
+			utils.on('initialised', oAdsInitSpy);
+
+			ads.init();
+
+			const eventDetail = oAdsInitSpy.args[0][0].detail;
+			expect(oAdsInitSpy.callCount).to.equal(1);
+			expect(eventDetail).to.equal(ads);
+			expect(ads.isInitialised).to.equal(true);
+
+		});
+
+		it('initAll() function is only triggered once', () => {
+			const ads = new Ads();
+			const gptInit = cy.spy(ads.gpt, 'init');
+
+			expect(gptInit.callCount).to.equal(0);
+
+			document.dispatchEvent(new Event('o.DOMContentLoaded'));
+			expect(gptInit.calledOnce);
+
+			document.dispatchEvent(new Event('o.DOMContentLoaded'));
+			expect(gptInit.calledOnce);
+		});
+
+		it('Manual inits always trigger but DOM inits do not override', () => {
+			const ads = new Ads();
+			const gptInit = cy.spy(ads.gpt, 'init');
+
+			expect(gptInit.callCount).to.equal(0);
+
+			ads.init();
+			expect(gptInit.calledOnce);
+
+			document.dispatchEvent(new Event('o.DOMContentLoaded'));
+			expect(gptInit.calledOnce);
+
+			ads.init();
+			expect(gptInit.calledTwice);
+		});
+	});
 });
--- cypress/tests/unit/sample-unit.spec.js
+++
@@ -1,9 +0,0 @@
-describe('My First Test', function() {
-	it('Does not do much!', function() {
-	  expect(true).to.equal(true);
-	})
-
-	it('Visits the Kitchen Sink', function() {
-		cy.visit('https://example.cypress.io')
-	})
-  })
