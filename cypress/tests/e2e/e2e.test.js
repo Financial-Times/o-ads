287581f679a8b04c0b1ed4098ff6d65a540fc997
Alex Florisca <alexflorisca@gmail.com>
2018-10-15 17:34:40 -0500

    Add comments

--- cypress/tests/e2e/e2e.test.js
+++ cypress/tests/e2e/e2e.test.js
@@ -1,7 +1,9 @@
-
+// E2E test should check the contents of the ad are what we expect.
+// We only need one or two of these to make sure nothing has changed on
+// the DFP side without us knowning.
 describe("Ad loads", () => {
 	it("Verify the leaderboard advert is displayed", () => {
-		cy.visit("/demos/local/Individual-Ad.html").as('IndividualAd');
+		cy.visit("/demos/local/test-ad.html").as('IndividualAd');
 		cy.get('#leaderboard-gpt').should('be.visible');
 		// Wait for the iframe to load
 		// Note - the iframe doesn't have an "src" attribute, it's populated
