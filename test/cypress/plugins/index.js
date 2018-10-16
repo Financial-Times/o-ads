7c4a56bde212906759952fa991aed33458dd43a0
Alex Florisca <alexflorisca@gmail.com>
2018-10-16 16:29:15 -0500

    use webpack-processor 2

--- test/cypress/plugins/index.js
+++ test/cypress/plugins/index.js
@@ -15,8 +15,6 @@ const webpack = require('@cypress/webpack-preprocessor');
 
 module.exports = (on) => {
 	const options = {
-	// send in the options from your webpack.config.js, so it works the same
-	// as your app's code
 		webpackOptions: require('origami-build-tools/config/webpack.config.dev'),
 		watchOptions: {}
 	};
