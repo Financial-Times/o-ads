9259918d3ccce68e74c6f38bc9322373bc5e00ba
Alex Florisca <alexflorisca@gmail.com>
2018-10-16 16:06:10 -0500

    File structure ok

---
+++ test/cypress/examples/unit/main.test.js
@@ -0,0 +1,78 @@
+const Ads = require('../../../main.js').constructor;
+const utils = require('../../../src/js/utils');
+
+describe('Main', () => {
+	beforeEach(() => {
+		cy.clearCookie('FTConsent');
+	});
+
+	it('oAds.ready event fires after o.DOMContentLoaded', () => {
+		document.body.insertAdjacentHTML('beforeend', '<div data-o-ads-name="banlb2" data-o-ads-formats="MediumRectangle"></div>');
+		new Ads();
+		const oAdsReadySpy = cy.spy();
+		utils.on('ready', oAdsReadySpy);
+
+		/*
+			cy runs two iframes. One loads the test, the other loads
+			a locally running app (if set).
+
+			window.document (here, refers to the test container)
+			cy.document() refers to the app env
+		*/
+		document.dispatchEvent(new Event('o.DOMContentLoaded'));
+
+		// Wait for event loop to finish
+		cy.wait(0).then(() => {
+			const eventDetail = oAdsReadySpy.args[0][0].detail;
+			expect(oAdsReadySpy.callCount).to.equal(1);
+			expect(eventDetail.name).to.equal('banlb2');
+			expect(eventDetail.slot.sizes).to.deep.equal([[300, 250]]);
+		});
+	});
+
+
+	context('Initialising', () => {
+		it('oAds.initialised event is triggered', () => {
+			const ads = new Ads();
+			const oAdsInitSpy = cy.spy();
+			utils.on('initialised', oAdsInitSpy);
+
+			ads.init();
+
+			const eventDetail = oAdsInitSpy.args[0][0].detail;
+			expect(oAdsInitSpy.callCount).to.equal(1);
+			expect(eventDetail).to.equal(ads);
+			expect(ads.isInitialised).to.equal(true);
+
+		});
+
+		it('initAll() function is only triggered once', () => {
+			const ads = new Ads();
+			const gptInit = cy.spy(ads.gpt, 'init');
+
+			expect(gptInit.callCount).to.equal(0);
+
+			document.dispatchEvent(new Event('o.DOMContentLoaded'));
+			expect(gptInit.calledOnce);
+
+			document.dispatchEvent(new Event('o.DOMContentLoaded'));
+			expect(gptInit.calledOnce);
+		});
+
+		it('Manual inits always trigger but DOM inits do not override', () => {
+			const ads = new Ads();
+			const gptInit = cy.spy(ads.gpt, 'init');
+
+			expect(gptInit.callCount).to.equal(0);
+
+			ads.init();
+			expect(gptInit.calledOnce);
+
+			document.dispatchEvent(new Event('o.DOMContentLoaded'));
+			expect(gptInit.calledOnce);
+
+			ads.init();
+			expect(gptInit.calledTwice);
+		});
+	});
+});
---
+++ test/cypress/integration/ad-requests.test.js
@@ -0,0 +1,41 @@
+function getQueryParams(url) {
+	console.log('url', url.slice(url.indexOf('?')+1));
+	const o = {};
+	url.slice(url.indexOf('?')+1)
+		.split('&')
+		.forEach(p => {
+			const key = p.slice(0, p.indexOf('='));
+			const value = p.slice(p.indexOf('=')+1);
+			o[decodeURIComponent(key)] = decodeURIComponent(value);
+		});
+	return o;
+}
+
+// Integration tests check the ad requests are made properly
+// We can spy on the ad calls and assert on the underlying xhr
+// object, including query parameters and response headers.
+describe('Integration tests', () => {
+	context('Simple ad', () => {
+		it('makes a request to the correct url', () => {
+			// Spy on the ad call with cy.server() and cy.route()
+			// see https://docs.cypress.io/guides/guides/network-requests.html for more details
+			// Note: this only works with xhr not fetch at the mo - there are workarounds for fetch
+			cy.server();
+			cy.route('https://securepubads.g.doubleclick.net/gampad/**').as('AdLoaded');
+			cy.visit("/demos/local/test-ad.html");
+
+			// Wait for the ad call to be made, then assert on the query string
+			cy.wait('@AdLoaded').then(xhr => {
+				const qsp = getQueryParams(xhr.url);
+				const cust_params = getQueryParams(qsp.cust_params);
+				console.log('qsp', qsp);
+				console.log('cust_params', cust_params);
+
+				// Size is set correctly
+				expect(qsp.sz).to.equal("728x90");
+				// Targeting is set correctly
+				expect(qsp.scp).to.equal("pos=top");
+			});
+		});
+	});
+});
