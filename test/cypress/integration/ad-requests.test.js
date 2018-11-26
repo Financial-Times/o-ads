86c96257b682cca527df1680517eeb39f8ab734a
Alex Florisca <alexflorisca@gmail.com>
2018-11-26 15:58:33 -0600

    Let's see if it works in CI

--- test/cypress/integration/ad-requests.test.js
+++ test/cypress/integration/ad-requests.test.js
@@ -12,7 +12,7 @@ function getQueryParams(url) {
 // We can spy on the ad calls and assert on the underlying xhr
 // object, including query parameters and response headers.
 describe('Integration tests', () => {
-	it.skip('Ad makes a request to the correct url', () => {
+	it('Ad call contains correct size, targeting and custom parameters specified by dfp_targeting config option', () => {
 		// Spy on the ad call with cy.server() and cy.route()
 		// see https://docs.cypress.io/guides/guides/network-requests.html for more details
 		// Note: this only works with xhr not fetch at the mo - there are workarounds for fetch
@@ -41,7 +41,7 @@ describe('Integration tests', () => {
 	});
 
 	// NOT WORKING - ads requests never fire when responsive formats are set - find out why
-	it('Responsive ad renders correctly', () => {
+	it.skip('Responsive ad renders correctly', () => {
 		cy.server();
 		cy.route('https://securepubads.g.doubleclick.net/gampad/**').as('AdLoaded');
 		cy.visit("/demos/local/test-ad-responsive.html");
@@ -65,6 +65,31 @@ describe('Integration tests', () => {
 			const url = xhr.xhr.responseURL;
 			const qsp = getQueryParams(url);
 			console.log("Loaded");
+		});
+	});
+
+	it('Targeting API ads the correct values to the ad call', () => {
+		cy.server();
+		cy.route('https://securepubads.g.doubleclick.net/gampad/**').as('AdLoaded');
+		cy.visit("/demos/local/Individual-Ad-with-Custom-Targeting-Values.html");
+
+		cy.wait('@AdLoaded').then(xhr => {
+			const url = xhr.xhr.responseURL;
+			const qsp = getQueryParams(url);
+			const cust_params = getQueryParams(qsp.cust_params);
+			console.log('cust_params', cust_params);
+			// user props
+			// TODO: Work out how to sign in a user before this ad call
+			expect(cust_params).to.have.property('slv', 'anon');
+			// This SHOULD be there, but I think is stripped out by GPT.
+			// To investigate on this card: https://trello.com/c/IbpPFURi/314-the-loggedin-parameter-never-actually-makes-it-into-the-ad-call-because-its-a-boolean
+			// expect(cust_params).to.have.property('loggedIn', 'false');
+
+			// Content props
+			expect(cust_params).to.have.property('auuid', '047b1294-75a9-11e6-b60a-de4532d5ea35');
+			expect(cust_params).to.have.property('ad', 'bs06,bs07,bs11,e2,ft02,ft04,ft05,ft11,ft18,ft35,ft38,ft41,g1,s03');
+			expect(cust_params).to.have.property('ca', 'business,finance,smartphone');
+			expect(cust_params).to.have.property('topic', 'Retail,US & Canadian companies,Technology sector,Retail & Consumer,Companies,Mobile devices,Brexit');
 		});
 	});
 });
