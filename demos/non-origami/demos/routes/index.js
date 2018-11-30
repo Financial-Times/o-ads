d9cbe169823e32e3a86b8a4e916626247607f76e
Carles Andres <carlos.andres.c@gmail.com>
2018-11-30 14:38:34 +0000

    Fix eslint for non-origami demos

--- demos/non-origami/demos/routes/index.js
+++ demos/non-origami/demos/routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router(); //eslint-disable-line new-cap
 
 /* GET home page. */
 router.get('/', function(req, res) {
