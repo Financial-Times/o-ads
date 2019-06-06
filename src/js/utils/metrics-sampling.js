4275ef2d39d92ed09107c5c71bb475a7d2738567
Carles Andres <carlos.andres.c@gmail.com>
2019-06-06 11:14:32 +0100

    Undo unnecessary change

--- src/js/utils/metrics-sampling.js
+++
@@ -1,9 +0,0 @@
-import config from '../config';
-
-const metricsSampleThreshold = Math.random();
-
-export default function inSample(sampleSize) {
-	return typeof sampleSize === 'undefined' || config.disableMetricsSampling || sampleSize > metricsSampleThreshold;
-}
-
-
