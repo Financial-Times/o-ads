2239d7918e07001548c3a7112e54cbbe026ca8a0
Carles Andres <carlos.andres.c@gmail.com>
2019-08-07 21:04:54 +0100

    Added eslint cofig for puppeteer

--- test/puppeteer/main.js
+++ test/puppeteer/main.js
@@ -7,15 +7,15 @@ const getConfig = () => {
 		devtools: true
 	};
 
-	// Once the PoC is working we can decide to return 
+	// Once the PoC is working we can decide to return
 	// different configs for different scnearios
 	return debugging_mode;
 };
 
 describe('on page load', () => {
 	test('h1 loads correctly', async () => {
-		let browser = await puppeteer.launch(getConfig());
-		let page = await browser.newPage();
+		const browser = await puppeteer.launch(getConfig());
+		const page = await browser.newPage();
 
 		await page.goto('http://localhost:3002/demos/local/Individual-Ad.html');
 		const iframeSelector = '#google_ads_iframe_/5887/test.5887.origami_0';
