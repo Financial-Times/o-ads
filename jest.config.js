991c829d0c575be8863226579647eb7d4687c3f9
Carles Andres <carlos.andres.c@gmail.com>
2019-08-21 16:17:30 +0100

    Added e2e task to CircleCI test task

--- jest.config.js
+++ jest.config.js
@@ -89,7 +89,7 @@ module.exports = {
 	// notifyMode: "failure-change",
 
 	// A preset that is used as a base for Jest's configuration
-	preset: "jest-puppeteer",
+	// preset: null,
 
 	// Run tests from one or more projects
 	// projects: null,
