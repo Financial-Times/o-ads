78a030a7a33022f6a499243aa23b0c8721fe9a57
Carles Andres <carlos.andres.c@gmail.com>
2019-08-21 14:04:29 +0100

    First Poc for testing network requests

--- test/puppeteer/main.js
+++ test/puppeteer/main.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const parseUrl = require('parse-url');
 
 const getConfig = () => {
 	const debugging_mode = {
@@ -12,15 +13,45 @@ const getConfig = () => {
 	return debugging_mode;
 };
 
-describe('on page load', () => {
-	test('h1 loads correctly', async () => {
-		const browser = await puppeteer.launch(getConfig());
-		const page = await browser.newPage();
+describe('Individual Ad', () => {
+	let browser;
+	let page;
 
-		await page.goto('http://localhost:3002/demos/local/Individual-Ad.html');
-		const iframeSelector = '#google_ads_iframe_/5887/test.5887.origami_0';
+	beforeAll(async () => {
+		browser = await puppeteer.launch(getConfig());
+		page = await browser.newPage();
+
+		page.goto('http://localhost:3002/demos/local/Individual-Ad.html');
+	}, 6000);
+
+	test('makes a network request to DFP', async () => {
+		const promise = new Promise((resolve, reject) => {
+			page.on('requestfinished', (request) => {
+				const requestedUrl = request.url();
+				// We don't look at other requests made by the same page
+				if (requestedUrl.match(/securepubads.g.doubleclick.net\/gampad/)) {
+					const urlObj = parseUrl(requestedUrl);
+					const { query } = urlObj;
+					const expectedParams = ['cust_params'];
+					expectedParams.forEach( param => {
+						// expect(true).toBe(true);
+						try {
+							expect(query).toHaveProperty(param);
+						} catch(err) {
+							reject(err);
+						}
+					});
+					resolve(true);
+				}
+			});
+		}, 10000);
+		await promise;
+	});
+
+	test('loads correctly an ad correctly', async () => {
+		const iframeSelector = '.o-ads__inner';
 		const html = await page.$eval(iframeSelector, e => e.innerHTML );
-		expect(html).toBe('a');
+		expect(html).toMatch('id="google_ads_iframe_/5887/test.5887.origami_0__container__"');
 		browser.close();
 	});
 });
