7cf44b8eab074b89b3eb75cac01a819efe27b601
Carles Andrés <carlos.andres.c@gmail.com>
2019-09-05 11:03:30 +0100

    Enable class injection based on the new o-ads-embed postMessage (#304)
    * Initial work to enable class injection based on the new o-ads-embed postMessage
    * Fixed adding a class
    * Added unit test for new addClass functionality
    * Added test for slot.addClass method
    * Added eslint rule for preventing Qunit.only
    * Update src/js/slot.js

---
+++ test/jest/slot.js
@@ -0,0 +1,22 @@
+/* eslint-env jest */
+
+import '@testing-library/jest-dom/extend-expect';
+import Slot from '../../src/js/slot.js';
+
+test('slot to export a "Slot" object', () => {
+	expect(Slot).toBeDefined();
+});
+
+test('slot.addClass adds a class to the slot container', (done) => {
+	  document.body.innerHTML = '<div class="container"></div>';
+
+	const container = document.querySelector('.container');
+	const newSlot = new Slot(container);
+	newSlot.addClass('sticky');
+
+	setTimeout( () => {
+		expect(container).toHaveClass('o-ads-sticky');
+		done();
+	}, 0);
+});
+
