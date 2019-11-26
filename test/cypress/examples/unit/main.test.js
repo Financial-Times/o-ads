7b3f45fb1052ecd29fa50cddc77e5ec3091b5a93
Andrew Georgiou <andrewgeorgiou1981@gmail.com>
2019-11-26 17:31:04 +0000

    Cypress tests that check network requests to DFP were failing as the request schema seems to have changed on Google's side

--- test/cypress/examples/unit/main.test.js
+++ test/cypress/examples/unit/main.test.js
@@ -1,23 +1,3 @@
-(function () {
-	const prefix = document.webkitVisibilityState ? 'webkit' : document.mozVisibilityState ? 'moz' : null;
-	function normalizeState () {
-		document.hidden = document[prefix + 'Hidden'];
-		document.visibilityState = document[prefix + 'VisibilityState'];
-	}
-
-	if (!prefix) {
-		return;
-	}
-
-	normalizeState();
-
-	document.addEventListener(prefix + 'visibilitychange', function () {
-		normalizeState();
-		document.dispatchEvent(new CustomEvent('visibilitychange'));
-	});
-
-}());
-
 /* globals describe, it, cy, expect: false: false */
 const Ads = require('../../../main.js').constructor;
 const utils = require('../../../src/js/utils');
