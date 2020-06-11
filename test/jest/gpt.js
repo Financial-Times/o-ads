ce733cff8fcb28a326081b7dfec513c235e00413
alevito <alessandro.vito84@gmail.com>
2020-06-11 17:20:15 +0100

    ADSDEV-310 Add jest unit test for enableLazyLoad()

--- test/jest/gpt.js
+++ test/jest/gpt.js
@@ -1,31 +1,67 @@
 /* eslint-env jest */
 /* global jsdom */
 
-import gpt from '../../src/js/ad-servers/gpt';
+import gpt, { DEFAULT_LAZY_LOAD } from '../../src/js/ad-servers/gpt';
+import GoogleTagMock from './testUtils/GoogleTagMock';
+
+jest.mock('./testUtils/GoogleTagMock');
 
 describe('setup', () => {
-	it('strips search terms', () => {
-		// @ts-ignore jsdom
-		jsdom.reconfigure({
-			url: 'https://www.ft.com/search?q=goog'
+	beforeEach(() => {
+		window.googletag = new GoogleTagMock();
+	});
+
+	describe ('setTargeting()', () => {
+
+		test('should strip search terms', () => {
+			// @ts-ignore jsdom
+			jsdom.reconfigure({
+				url: 'https://www.ft.com/search?q=goog'
+			});
+
+			gpt.setup({});
+
+			expect(global.googletag.pubads().setTargeting)
+				.toBeCalledWith('url', 'https://www.ft.com/search');
 		});
 
-		const setTargetingSpy = jest.fn();
-
-		// @ts-ignore googletag
-		window.googletag = {
-			cmd: { push: jest.fn() },
-			enableServices: jest.fn(),
-			pubads: () => ({
-				setTargeting: setTargetingSpy,
-				addEventListener: jest.fn(),
-				setRequestNonPersonalizedAds: jest.fn(),
-				enableAsyncRendering: jest.fn()
-			})
-		};
-
-		gpt.setup({});
-
-		expect(setTargetingSpy).toBeCalledWith('url', 'https://www.ft.com/search');
 	});
+
+	describe('enableLazyLoad()', () => {
+
+		test('should explicitly disable lazy load', () => {
+			gpt.setup({
+				enableLazyLoad: false,
+			});
+
+			expect(global.googletag.pubads().enableLazyLoad)
+				.not.toBeCalled();
+		});
+
+		test('should enable lazy load with default parameters', () => {
+			gpt.setup({
+				enableLazyLoad: true,
+			});
+
+			expect(global.googletag.pubads().enableLazyLoad)
+				.toBeCalledWith(DEFAULT_LAZY_LOAD);
+		});
+
+		test('should enable lazy load with custom parameters', () => {
+			const gptConfig = {
+				enableLazyLoad: {
+					fetchMarginPercent: 300,
+					renderMarginPercent: 200,
+				}
+			};
+			gpt.setup(gptConfig);
+
+			expect(global.googletag.pubads().enableLazyLoad)
+				.toBeCalledWith(
+					Object.assign({}, DEFAULT_LAZY_LOAD, gptConfig.enableLazyLoad)
+				);
+		});
+
+	});
+
 });
