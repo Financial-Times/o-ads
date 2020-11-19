6019372360a2ae3f79eb44a3064f74c082e88767
Alex Florisca <alexflorisca@gmail.com>
2020-11-19 10:50:11 +0000

    Remove browser field

--- jest.config.js
+++ jest.config.js
@@ -8,9 +8,6 @@ module.exports = {
 	// Stop running tests after `n` failures
 	// bail: 0,
 
-	// Respect "browser" field in package.json when resolving modules
-	browser: true,
-
 	// The directory where Jest should store its cached dependency information
 	// cacheDirectory: "/private/var/folders/mm/8xxt_jqd5bb4kjh_v0gh8v4h0000gn/T/jest_dx",
 
