3e948fb05db7c491dbabea26a9568ed62ea5583a
gitGremlin <mo.shawwa@ft.com>
2021-02-09 15:08:46 +0000

    covering enableLazyLoad

--- src/js/ad-servers/gpt.js
+++ src/js/ad-servers/gpt.js
@@ -37,7 +37,7 @@ function init() {
 	utils.on('slotCanRender', onRender);
 	utils.on('refresh', onRefresh);
 	utils.on('resize', onResize);
-	if(gptConfig.rendering !== 'sra') { googletag.cmd.push(() => setup(gptConfig)); }
+	if(gptConfig.rendering !== 'sra' || gptConfig.enableLazyLoad) { googletag.cmd.push(() => setup(gptConfig)); }
 }
 
 /*
@@ -246,14 +246,14 @@ function onReady(slotMethods, gptConfig, event) {
 				.setCollapseEmpty()
 				.setTargeting()
 				.setURL();
-			if(gptConfig.rendering === 'sra' && adCountArray.indexOf(slot) === adCountArray.length-1) {
+			if(gptConfig.rendering === 'sra' && !gptConfig.enableLazyLoad && adCountArray.indexOf(slot) === adCountArray.length-1) {
 				googletag.cmd.push(() => setup(gptConfig));
 			}
 			if (!slot.defer && slot.hasValidSize()) {
 				slot.display();
 			}
 		});
-	}	else if (gptConfig.rendering === 'sra' && adCountArray.indexOf(slot) === adCountArray.length-1) {
+	}	else if (gptConfig.rendering === 'sra' && !gptConfig.enableLazyLoad && adCountArray.indexOf(slot) === adCountArray.length-1) {
 		googletag.cmd.push(() => setup(gptConfig));
 	}
 }
